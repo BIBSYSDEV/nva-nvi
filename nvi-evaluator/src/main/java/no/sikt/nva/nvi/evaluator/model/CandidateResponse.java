2bea97534d996e7999689a5fa904d13c8171d2e3
Haakon Heiberg <haakon.heiberg@sikt.no>
2023-06-16 16:08:10 +0200

    Bob the builder has left the building

--- nvi-evaluator/src/main/java/no/sikt/nva/nvi/evaluator/model/CandidateResponse.java
+++ nvi-evaluator/src/main/java/no/sikt/nva/nvi/evaluator/model/CandidateResponse.java
@@ -1,11 +1,8 @@
 package no.sikt.nva.nvi.evaluator.model;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class CandidateResponse {
 
@@ -17,35 +14,11 @@ public class CandidateResponse {
         this.approvalAffiliations = approvalAffiliations;
     }
 
-    public static Builder builder() {
-        return new Builder();
-    }
-
     public URI getPublicationId() {
         return publicationId;
     }
 
     public Collection<URI> getApprovalAffiliations() {
         return approvalAffiliations;
-    }
-
-    public static class Builder {
-
-        private URI publicationId;
-        private final Set<URI> approvalAffiliations = new HashSet<>();
-
-        public Builder withPublicationId(URI publicationId) {
-            this.publicationId = publicationId;
-            return this;
-        }
-
-        public Builder withApprovalAffiliations(Collection<URI> approvalAffiliationUris) {
-            this.approvalAffiliations.addAll(approvalAffiliationUris);
-            return this;
-        }
-
-        public CandidateResponse build() {
-            return new CandidateResponse(publicationId, new ArrayList<>(approvalAffiliations));
-        }
     }
 }
