a5f1e285f4c69884377ef7676e701068a4dcfd38
Haakon Heiberg <haakon.heiberg@sikt.no>
2023-06-21 09:10:37 +0200

    Add logging to bucket reader

--- nvi-evaluator/src/main/java/no/sikt/nva/nvi/evaluator/aws/S3StorageReader.java
+++ nvi-evaluator/src/main/java/no/sikt/nva/nvi/evaluator/aws/S3StorageReader.java
@@ -6,15 +6,18 @@ import no.unit.nva.events.models.EventReference;
 import no.unit.nva.s3.S3Driver;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
-import nva.commons.core.paths.UnixPath;
 import nva.commons.core.paths.UriWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.services.s3.S3Client;
 
 public class S3StorageReader implements StorageReader<EventReference> {
+
     private static final String EXPANDED_RESOURCES_BUCKET = new Environment().readEnv(
         "EXPANDED_RESOURCES_BUCKET");
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(S3StorageReader.class);
     private final S3Client client;
 
     public S3StorageReader() {
@@ -28,7 +31,9 @@ public class S3StorageReader implements StorageReader<EventReference> {
     @Override
     public String read(EventReference blob) {
         var resourceRelativePath = UriWrapper.fromUri(blob.getUri()).toS3bucketPath();
+        LOGGER.info("Getting s3 path for file {}", resourceRelativePath.toString());
         var s3Driver = new S3Driver(client, EXPANDED_RESOURCES_BUCKET);
+        LOGGER.info("S3 driver initialized");
         return s3Driver.getFile(resourceRelativePath);
     }
 
