9d6feed8886d976f9284f1811c18124e7bab465b
monaullah <monaullah98@gmail.com>
2023-06-26 15:25:13 +0200

    OpenSearchIndexClient create index if it doesnt exist

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/aws/OpenSearchIndexClient.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/aws/OpenSearchIndexClient.java
@@ -6,6 +6,8 @@ import no.sikt.nva.nvi.index.model.NviCandidateIndexDocument;
 import nva.commons.core.JacocoGenerated;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.opensearch.core.IndexRequest;
+import org.opensearch.client.opensearch.indices.CreateIndexRequest;
+import org.opensearch.client.opensearch.indices.ExistsRequest;
 import org.opensearch.client.transport.aws.AwsSdk2Transport;
 import org.opensearch.client.transport.aws.AwsSdk2TransportOptions;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
@@ -15,7 +17,7 @@ import software.amazon.awssdk.regions.Region;
 //TODO: Handle test coverage
 public class OpenSearchIndexClient implements IndexClient<NviCandidateIndexDocument> {
 
-    public static final String INDEX = "nviCandidates";
+    private static final String INDEX = "nviCandidates";
     private final OpenSearchClient openSearchClient;
 
     public OpenSearchIndexClient(String openSearchEndpoint, Region region) {
@@ -27,6 +29,9 @@ public class OpenSearchIndexClient implements IndexClient<NviCandidateIndexDocum
                 AwsSdk2TransportOptions.builder().build()
             )
         );
+        if (!indexExists()) {
+            createIndex();
+        }
     }
 
     @Override
@@ -37,8 +42,20 @@ public class OpenSearchIndexClient implements IndexClient<NviCandidateIndexDocum
     private static IndexRequest<NviCandidateIndexDocument> constructIndexRequest(
         NviCandidateIndexDocument indexDocument) {
         return new IndexRequest.Builder<NviCandidateIndexDocument>().index(INDEX)
-                   .id(indexDocument.identifier())
+                   .id(indexDocument.getIdentifier())
                    .document(indexDocument)
                    .build();
+    }
+
+    private boolean indexExists() {
+        return attempt(
+            () -> openSearchClient.indices()
+                      .exists(ExistsRequest.of(s -> s.index(OpenSearchIndexClient.INDEX)))
+                      .value()).orElseThrow();
+    }
+
+    private void createIndex() {
+        attempt(() -> openSearchClient.indices().create(new CreateIndexRequest.Builder().index(
+            OpenSearchIndexClient.INDEX).build())).orElseThrow();
     }
 }
