7a9dda37531d78399e16112277310a55156766cb
monaullah <monaullah98@gmail.com>
2023-08-09 13:07:26 +0200

    Remove unused method in IndexNviCandidateHandler

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/IndexNviCandidateHandler.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/IndexNviCandidateHandler.java
@@ -1,9 +1,6 @@
 package no.sikt.nva.nvi.index;
 
 import static java.util.Objects.isNull;
-import static no.sikt.nva.nvi.common.ApplicationConstants.REGION;
-import static no.sikt.nva.nvi.common.ApplicationConstants.SEARCH_INFRASTRUCTURE_API_HOST;
-import static no.sikt.nva.nvi.common.ApplicationConstants.SEARCH_INFRASTRUCTURE_AUTH_URI;
 import static no.sikt.nva.nvi.index.aws.OpenSearchClient.defaultOpenSearchClient;
 import static no.sikt.nva.nvi.index.utils.NviCandidateIndexDocumentGenerator.generateNviCandidateIndexDocument;
 import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
@@ -12,23 +9,14 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.time.Clock;
 import java.util.Objects;
 import no.sikt.nva.nvi.common.StorageReader;
-import no.sikt.nva.nvi.common.model.UsernamePasswordWrapper;
-import no.sikt.nva.nvi.index.aws.OpenSearchClient;
 import no.sikt.nva.nvi.index.aws.S3StorageReader;
 import no.sikt.nva.nvi.index.aws.SearchClient;
 import no.sikt.nva.nvi.index.model.NviCandidateIndexDocument;
 import no.sikt.nva.nvi.index.model.NviCandidateMessageBody;
-import no.unit.nva.auth.CachedJwtProvider;
-import no.unit.nva.auth.CognitoAuthenticator;
-import no.unit.nva.auth.CognitoCredentials;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
-import nva.commons.secrets.SecretsReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,7 +26,6 @@ public class IndexNviCandidateHandler implements RequestHandler<SQSEvent, Void> 
     private static final Environment ENVIRONMENT = new Environment();
     private static final String EXPANDED_RESOURCES_BUCKET = ENVIRONMENT.readEnv(
         "EXPANDED_RESOURCES_BUCKET");
-    public static final String SEARCH_INFRASTRUCTURE_CREDENTIALS = "SearchInfrastructureCredentials";
     private static final String ERROR_MESSAGE_BODY_INVALID = "Message body invalid: {}";
     private final SearchClient<NviCandidateIndexDocument> indexClient;
     private final StorageReader<NviCandidateMessageBody> storageReader;
@@ -66,14 +53,6 @@ public class IndexNviCandidateHandler implements RequestHandler<SQSEvent, Void> 
             .forEach(this::addNviCandidateToIndex);
 
         return null;
-    }
-
-    @JacocoGenerated
-    private static CognitoCredentials createCognitoCredentials(SecretsReader secretsReader) {
-        var credentials = secretsReader.fetchClassSecret(SEARCH_INFRASTRUCTURE_CREDENTIALS,
-                                                         UsernamePasswordWrapper.class);
-        return new CognitoCredentials(credentials::getUsername, credentials::getPassword,
-                                      URI.create(SEARCH_INFRASTRUCTURE_AUTH_URI));
     }
 
     private void addNviCandidateToIndex(NviCandidateMessageBody candidate) {
