dec701945ad08f6530791bdd26ce5d732d479ce9
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2023-09-20 13:30:04 +0200

    CandidateResponse Note user type changed to String and bug fix (#98)
    * CandidateResponse Note user type changed to String
    
    * Fix Note var name according to docs

--- nvi-rest/src/main/java/no/sikt/nva/nvi/rest/fetch/Note.java
+++ nvi-rest/src/main/java/no/sikt/nva/nvi/rest/fetch/Note.java
@@ -3,13 +3,11 @@ package no.sikt.nva.nvi.rest.fetch;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import java.time.Instant;
-import no.sikt.nva.nvi.common.db.model.DbUsername;
-import nva.commons.core.JacocoGenerated;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonSerialize
-@JacocoGenerated // TODO not in use yet
-public record Note(DbUsername user,
+public record Note(String user,
                    String text,
                    Instant createdDate) {
+
 }
