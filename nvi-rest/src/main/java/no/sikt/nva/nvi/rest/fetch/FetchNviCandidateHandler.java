4a5cb04457f0596d03346bb1ee0b866238d39c83
monaullah <monaullah98@gmail.com>
2023-09-27 14:17:59 +0200

    Move logic to NviService

--- nvi-rest/src/main/java/no/sikt/nva/nvi/rest/fetch/FetchNviCandidateHandler.java
+++ nvi-rest/src/main/java/no/sikt/nva/nvi/rest/fetch/FetchNviCandidateHandler.java
@@ -5,7 +5,6 @@ import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
 import java.util.Optional;
 import java.util.UUID;
-import no.sikt.nva.nvi.common.service.Candidate;
 import no.sikt.nva.nvi.common.service.NviService;
 import no.sikt.nva.nvi.rest.model.CandidateResponse;
 import no.sikt.nva.nvi.rest.model.CandidateResponseMapper;
@@ -13,13 +12,11 @@ import no.sikt.nva.nvi.utils.ExceptionMapper;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
-import nva.commons.apigateway.exceptions.NotFoundException;
 import nva.commons.core.JacocoGenerated;
 
 public class FetchNviCandidateHandler extends ApiGatewayHandler<Void, CandidateResponse> {
 
     public static final String CANDIDATE_IDENTIFIER = "candidateIdentifier";
-    public static final String ERROR_MSG_CANDIDATE_NOT_APPLICABLE = "Candidate is no longer applicable";
     private final NviService service;
 
     @JacocoGenerated
@@ -37,9 +34,8 @@ public class FetchNviCandidateHandler extends ApiGatewayHandler<Void, CandidateR
         throws ApiGatewayException {
         return attempt(() -> requestInfo.getPathParameter(CANDIDATE_IDENTIFIER))
                    .map(UUID::fromString)
-                   .map(service::findCandidateById)
+                   .map(service::findApplicableCandidateById)
                    .map(Optional::orElseThrow)
-                   .map(this::validateIfApplicable)
                    .map(CandidateResponseMapper::toDto)
                    .orElseThrow(ExceptionMapper::map);
     }
@@ -47,16 +43,5 @@ public class FetchNviCandidateHandler extends ApiGatewayHandler<Void, CandidateR
     @Override
     protected Integer getSuccessStatusCode(Void input, CandidateResponse output) {
         return HTTP_OK;
-    }
-
-    private static boolean isNotApplicable(Candidate candidate) {
-        return !candidate.candidate().applicable();
-    }
-
-    private Candidate validateIfApplicable(Candidate candidate) throws NotFoundException {
-        if (isNotApplicable(candidate)) {
-            throw new NotFoundException(ERROR_MSG_CANDIDATE_NOT_APPLICABLE);
-        }
-        return candidate;
     }
 }
