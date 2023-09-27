8211ffae6b4770f4663c64674b136311065a5dca
monaullah <monaullah98@gmail.com>
2023-09-27 13:05:55 +0200

    Test shouldReturnNotFoundIfCandidateExistsButNotApplicable. Removed mocking of nviService in tests

--- nvi-rest/src/test/java/no/sikt/nva/nvi/rest/fetch/FetchNviCandidateHandlerTest.java
+++ nvi-rest/src/test/java/no/sikt/nva/nvi/rest/fetch/FetchNviCandidateHandlerTest.java
@@ -2,60 +2,56 @@ package no.sikt.nva.nvi.rest.fetch;
 
 import static no.sikt.nva.nvi.rest.fetch.FetchNviCandidateHandler.CANDIDATE_IDENTIFIER;
 import static no.sikt.nva.nvi.test.TestUtils.randomCandidate;
-import static no.sikt.nva.nvi.test.TestUtils.randomCandidateBuilder;
 import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
-import java.time.Instant;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
-import no.sikt.nva.nvi.common.db.ApprovalStatusDao.DbApprovalStatus;
-import no.sikt.nva.nvi.common.db.CandidateDao.DbCandidate;
-import no.sikt.nva.nvi.common.db.PeriodStatus.Status;
 import no.sikt.nva.nvi.common.service.Candidate;
 import no.sikt.nva.nvi.common.service.NviService;
 import no.sikt.nva.nvi.rest.model.CandidateResponse;
 import no.sikt.nva.nvi.rest.model.CandidateResponseMapper;
+import no.sikt.nva.nvi.test.LocalDynamoTest;
 import no.unit.nva.testutils.HandlerRequestBuilder;
 import nva.commons.apigateway.GatewayResponse;
-import nva.commons.core.paths.UriWrapper;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.HttpStatus;
 import org.eclipse.jetty.http.HttpHeader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class FetchNviCandidateHandlerTest {
+class FetchNviCandidateHandlerTest extends LocalDynamoTest {
 
-    public static final Instant NOW = Instant.now();
     private static final Context CONTEXT = mock(Context.class);
     private ByteArrayOutputStream output;
     private FetchNviCandidateHandler handler;
-    private NviService service;
+    private NviService nviService;
 
     @BeforeEach
     public void setUp() {
-
         output = new ByteArrayOutputStream();
-        service = mock(NviService.class);
-        handler = new FetchNviCandidateHandler(service);
+        nviService = new NviService(initializeTestDatabase());
+        handler = new FetchNviCandidateHandler(nviService);
     }
 
     @Test
     void shouldReturn404whenCandidateDoesNotExist() throws IOException {
 
-        var candidateIdentifier = UUID.randomUUID();
-        var input = getInput(candidateIdentifier);
-        when(service.findCandidateById(candidateIdentifier)).thenReturn(Optional.empty());
+        handler.handleRequest(createRequest(UUID.randomUUID()), output, CONTEXT);
+        var gatewayResponse = getGatewayResponse();
+
+        assertEquals(HttpStatus.SC_NOT_FOUND, gatewayResponse.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnNotFoundIfCandidateExistsButNotApplicable() throws IOException {
+        var candidate = nviService.upsertCandidate(randomCandidate()).orElseThrow();
+        var input = createRequest(candidate.identifier());
         handler.handleRequest(input, output, CONTEXT);
         var gatewayResponse = getGatewayResponse();
 
@@ -64,27 +60,23 @@ class FetchNviCandidateHandlerTest {
 
     @Test
     void shouldReturnValidCandidateIfExists() throws IOException {
-        var candidateId = UUID.randomUUID();
-        var candidateWithIdentifier = getCandidate(candidateId, randomCandidate(), List.of());
-        when(service.findCandidateById(candidateId)).thenReturn(Optional.of(candidateWithIdentifier));
-        var input = getInput(candidateId);
+        var candidate = nviService.upsertCandidate(randomCandidate()).orElseThrow();
+        var input = createRequest(candidate.identifier());
         handler.handleRequest(input, output, CONTEXT);
+
         var gatewayResponse = getGatewayResponse();
         assertEquals(HttpStatus.SC_OK, gatewayResponse.getStatusCode());
-        var bodyObject = gatewayResponse.getBodyObject(CandidateResponse.class);
-        var candidateUri = constructId(candidateId);
-        assertEquals(bodyObject.id(), candidateUri);
-        var expectedResponse = getExpectedResponse(candidateWithIdentifier);
 
-        assertEquals(bodyObject, expectedResponse);
+        var expectedResponse = getExpectedResponse(candidate);
+        var actualResponse = gatewayResponse.getBodyObject(CandidateResponse.class);
+
+        assertEquals(actualResponse, expectedResponse);
     }
 
     @Test
     void shouldHandleNullNotes() throws IOException {
-        var candidateId = UUID.randomUUID();
-        var candidate = getCandidate(candidateId, randomCandidateBuilder(true).build(), List.of());
-        when(service.findCandidateById(candidateId)).thenReturn(Optional.of(candidate));
-        var input = getInput(candidateId);
+        var candidate = nviService.upsertCandidate(randomCandidate()).orElseThrow();
+        var input = createRequest(candidate.identifier());
         handler.handleRequest(input, output, CONTEXT);
         var gatewayResponse = getGatewayResponse();
         assertEquals(HttpStatus.SC_OK, gatewayResponse.getStatusCode());
@@ -98,23 +90,11 @@ class FetchNviCandidateHandlerTest {
         return CandidateResponseMapper.toDto(candidate);
     }
 
-    private static URI constructId(UUID identifier) {
-        return UriWrapper.fromHost("https://example.org")
-                   .addChild("scientific-index", "candidate")
-                   .addChild(identifier.toString())
-                   .getUri();
-    }
-
-    private static InputStream getInput(UUID publicationId) throws JsonProcessingException {
+    private static InputStream createRequest(UUID publicationId) throws JsonProcessingException {
         return new HandlerRequestBuilder<InputStream>(dtoObjectMapper)
                    .withHeaders(Map.of(HttpHeader.ACCEPT.asString(), ContentType.APPLICATION_JSON.getMimeType()))
                    .withPathParameters(Map.of(CANDIDATE_IDENTIFIER, publicationId.toString()))
                    .build();
-    }
-
-    private static Candidate getCandidate(UUID id, DbCandidate candidate, List<DbApprovalStatus> approvalStatusList) {
-        return new Candidate(id, candidate, approvalStatusList, List.of(),
-                             new no.sikt.nva.nvi.common.db.PeriodStatus(NOW, Status.OPEN_PERIOD));
     }
 
     private GatewayResponse<CandidateResponse> getGatewayResponse()
