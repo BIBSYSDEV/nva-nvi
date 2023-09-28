449ceddc7dd7f3fab092a220ea607fb973d94bbc
Kir Truhacev <kir.truhacev@sikt.no>
2023-09-28 19:05:45 +0200

    adding try catch block

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/UpdateIndexHandler.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/UpdateIndexHandler.java
@@ -21,6 +21,7 @@ import no.sikt.nva.nvi.index.model.NviCandidateIndexDocument;
 import no.sikt.nva.nvi.index.utils.NviCandidateIndexDocumentGenerator;
 import no.unit.nva.auth.uriretriever.AuthorizedBackendUriRetriever;
 import no.unit.nva.commons.json.JsonUtils;
+import nva.commons.apigateway.exceptions.NotFoundException;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 import org.slf4j.Logger;
@@ -29,6 +30,7 @@ import org.slf4j.LoggerFactory;
 @JacocoGenerated
 public class UpdateIndexHandler implements RequestHandler<DynamodbEvent, Void> {
 
+    public static final String COULD_NOT_UPDATE_INDEX_MESSAGE = "Could not update index for record: {}";
     private static final String CANDIDATE_TYPE = "CANDIDATE";
     private static final String APPROVAL_TYPE = "APPROVAL_STATUS";
     private static final String PRIMARY_KEY_DELIMITER = "#";
@@ -36,7 +38,6 @@ public class UpdateIndexHandler implements RequestHandler<DynamodbEvent, Void> {
     private static final Logger LOGGER = LoggerFactory.getLogger(UpdateIndexHandler.class);
     private static final Environment ENVIRONMENT = new Environment();
     private static final String EXPANDED_RESOURCES_BUCKET = ENVIRONMENT.readEnv("EXPANDED_RESOURCES_BUCKET");
-    public static final String COULD_NOT_UPDATE_INDEX_MESSAGE = "Could not update index for event: {}";
     private final SearchClient<NviCandidateIndexDocument> openSearchClient;
     private final StorageReader<URI> storageReader;
     private final NviService nviService;
@@ -48,10 +49,8 @@ public class UpdateIndexHandler implements RequestHandler<DynamodbEvent, Void> {
     }
 
     public UpdateIndexHandler(StorageReader<URI> storageReader,
-                              SearchClient<NviCandidateIndexDocument> openSearchClient,
-                              NviService nviService,
-                              NviCandidateIndexDocumentGenerator documentGenerator
-    ) {
+                              SearchClient<NviCandidateIndexDocument> openSearchClient, NviService nviService,
+                              NviCandidateIndexDocumentGenerator documentGenerator) {
         this.storageReader = storageReader;
         this.openSearchClient = openSearchClient;
         this.nviService = nviService;
@@ -59,15 +58,10 @@ public class UpdateIndexHandler implements RequestHandler<DynamodbEvent, Void> {
     }
 
     public Void handleRequest(DynamodbEvent event, Context context) {
-        try {
-            event.getRecords().stream()
-                .filter(this::isUpdate)
-                .filter(this::isCandidateOrApproval)
-                .forEach(this::updateIndex);
-        } catch (Exception e) {
-            var eventString = attempt(() -> JsonUtils.dtoObjectMapper.writeValueAsString(event)).orElseThrow();
-            LOGGER.error(COULD_NOT_UPDATE_INDEX_MESSAGE, eventString);
-        }
+        event.getRecords().stream()
+            .filter(this::isUpdate)
+            .filter(this::isCandidateOrApproval)
+            .forEach(this::updateIndex);
 
         return null;
     }
@@ -85,9 +79,7 @@ public class UpdateIndexHandler implements RequestHandler<DynamodbEvent, Void> {
     }
 
     private static NviCandidateIndexDocument toIndexDocumentWithId(UUID candidateIdentifier) {
-        return new NviCandidateIndexDocument.Builder()
-                   .withIdentifier(candidateIdentifier.toString())
-                   .build();
+        return new NviCandidateIndexDocument.Builder().withIdentifier(candidateIdentifier.toString()).build();
     }
 
     private static UUID extractIdentifierFromNewImage(DynamodbStreamRecord record) {
@@ -106,16 +98,26 @@ public class UpdateIndexHandler implements RequestHandler<DynamodbEvent, Void> {
         return CANDIDATE_TYPE.equals(extractRecordType(record));
     }
 
+    private static AuthorizedBackendUriRetriever defaultUriRetriver() {
+        return new AuthorizedBackendUriRetriever(new Environment().readEnv("BACKEND_CLIENT_AUTH_URL"),
+                                                 new Environment().readEnv("BACKEND_CLIENT_SECRET_NAME"));
+    }
+
     private boolean isCandidateOrApproval(DynamodbStreamRecord record) {
         return isCandidate(record) || isApproval(record);
     }
 
     private void updateIndex(DynamodbStreamRecord record) {
-        var candidate = nviService.findCandidateById(extractIdentifierFromNewImage(record)).orElseThrow();
-        if (isApplicable(candidate)) {
-            addDocumentToIndex(candidate);
-        } else {
-            removeDocumentFromIndex(candidate);
+        try {
+            var candidate = nviService.findCandidateById(extractIdentifierFromNewImage(record)).orElseThrow();
+            if (isApplicable(candidate)) {
+                addDocumentToIndex(candidate);
+            } else {
+                removeDocumentFromIndex(candidate);
+            }
+        } catch (Exception e) {
+            var eventString = attempt(() -> JsonUtils.dtoObjectMapper.writeValueAsString(record)).orElseThrow();
+            LOGGER.error(COULD_NOT_UPDATE_INDEX_MESSAGE, eventString);
         }
     }
 
@@ -126,23 +128,14 @@ public class UpdateIndexHandler implements RequestHandler<DynamodbEvent, Void> {
 
     private void removeDocumentFromIndex(Candidate candidate) {
         LOGGER.info("Attempting to remove document with identifier {}", candidate.identifier().toString());
-        attempt(candidate::identifier)
-            .map(UpdateIndexHandler::toIndexDocumentWithId)
+        attempt(candidate::identifier).map(UpdateIndexHandler::toIndexDocumentWithId)
             .forEach(openSearchClient::removeDocumentFromIndex)
             .orElseThrow();
     }
 
-    private static AuthorizedBackendUriRetriever defaultUriRetriver() {
-        return new AuthorizedBackendUriRetriever(
-            new Environment().readEnv("BACKEND_CLIENT_AUTH_URL"),
-            new Environment().readEnv("BACKEND_CLIENT_SECRET_NAME")
-        );
-    }
-
     private void addDocumentToIndex(Candidate candidate) {
         LOGGER.info("Attempting to add/update document with identifier {}", candidate.identifier().toString());
-        attempt(candidate::candidate)
-            .map(UpdateIndexHandler::extractBucketUri)
+        attempt(candidate::candidate).map(UpdateIndexHandler::extractBucketUri)
             .map(storageReader::read)
             .map(blob -> documentGenerator.generateDocument(blob, candidate))
             .forEach(openSearchClient::addDocumentToIndex)
