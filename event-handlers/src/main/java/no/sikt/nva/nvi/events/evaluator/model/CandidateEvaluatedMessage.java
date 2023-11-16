ab1442d97e3a33c685a5f82c8f48737c7ce8878c
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2023-11-16 12:26:30 +0100

    Refactor: Moved evaluator to module event-handlers (#175)

---
+++ event-handlers/src/main/java/no/sikt/nva/nvi/events/evaluator/model/CandidateEvaluatedMessage.java
@@ -0,0 +1,36 @@
+package no.sikt.nva.nvi.events.evaluator.model;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import no.sikt.nva.nvi.events.model.CandidateType;
+
+@JsonAutoDetect(fieldVisibility = Visibility.ANY)
+@JsonSerialize
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+public record CandidateEvaluatedMessage(
+    CandidateType candidate
+) {
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+
+        private CandidateType candidate;
+
+        private Builder() {
+        }
+
+        public Builder withCandidateType(CandidateType candidate) {
+            this.candidate = candidate;
+            return this;
+        }
+
+        public CandidateEvaluatedMessage build() {
+            return new CandidateEvaluatedMessage(candidate);
+        }
+    }
+}
---
+++ event-handlers/src/test/java/no/sikt/nva/nvi/events/evaluator/FakeStorageReader.java
@@ -0,0 +1,20 @@
+package no.sikt.nva.nvi.events.evaluator;
+
+import no.sikt.nva.nvi.common.StorageReader;
+import no.sikt.nva.nvi.events.evaluator.aws.S3StorageReader;
+import no.unit.nva.events.models.EventReference;
+import software.amazon.awssdk.services.s3.S3Client;
+
+class FakeStorageReader implements StorageReader<EventReference> {
+
+    private final StorageReader<EventReference> reader;
+
+    public FakeStorageReader(S3Client s3Client) {
+        this.reader = new S3StorageReader(s3Client);
+    }
+
+    @Override
+    public String read(EventReference blob) {
+        return reader.read(blob);
+    }
+}
