e574468e395738786abfae07c94725d6a29998c9
monaullah <monaullah98@gmail.com>
2023-11-21 12:32:37 +0100

    shouldFetchCandidatesByGivenYearAndPageSizeAndStartMarker. Added secondary index for year.

--- nvi-commons/src/test/java/no/sikt/nva/nvi/common/db/CandidateRepositoryTest.java
+++ nvi-commons/src/test/java/no/sikt/nva/nvi/common/db/CandidateRepositoryTest.java
@@ -1,15 +1,21 @@
 package no.sikt.nva.nvi.common.db;
 
-import static no.sikt.nva.nvi.test.TestUtils.randomCandidate;
 import static no.sikt.nva.nvi.test.TestUtils.randomCandidateBuilder;
+import static no.sikt.nva.nvi.test.TestUtils.randomIntBetween;
+import static no.sikt.nva.nvi.test.TestUtils.randomYear;
 import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.List;
+import java.util.stream.IntStream;
+import no.sikt.nva.nvi.common.db.CandidateDao.DbCandidate;
+import no.sikt.nva.nvi.common.db.CandidateDao.DbPublicationDate;
 import no.sikt.nva.nvi.test.LocalDynamoTest;
+import no.sikt.nva.nvi.test.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -35,7 +41,7 @@ class CandidateRepositoryTest extends LocalDynamoTest {
 
     @Test
     public void shouldOverwriteExistingCandidateWhenUpdating() {
-        var originalCandidate = randomCandidate();
+        var originalCandidate = TestUtils.randomCandidate();
         var created = candidateRepository.create(originalCandidate, List.of());
         var newCandidate = originalCandidate.copy().publicationBucketUri(randomUri()).build();
         candidateRepository.update(created.identifier(), newCandidate, List.of());
@@ -46,5 +52,28 @@ class CandidateRepositoryTest extends LocalDynamoTest {
         assertThat(fetched, is(equalTo(newCandidate)));
     }
 
+    @Test
+    void shouldFetchCandidatesByGivenYearAndPageSizeAndStartMarker() {
+        int year = Integer.parseInt(randomYear());
+        int numberOfCandidates = randomIntBetween(1, 10);
+        var candidates = createNumberOfCandidatesForYear(year, numberOfCandidates);
+        var results = candidateRepository.fetchCandidatesByYear(year, 5, null);
+        assertThat(results.size(), is(equalTo(numberOfCandidates)));
+        assertThat(results, containsInAnyOrder(candidates.toArray()));
+    }
 
+    private static DbCandidate randomCandidate(int year) {
+        return randomCandidateBuilder(true).publicationDate(publicationDate(year)).build();
+    }
+
+    private static DbPublicationDate publicationDate(int year) {
+        return new DbPublicationDate(String.valueOf(year), null, null);
+    }
+
+    private List<CandidateDao> createNumberOfCandidatesForYear(int year, int number) {
+        return IntStream.range(0, number)
+                   .mapToObj(i -> randomCandidate(year))
+                   .map(candidate -> candidateRepository.createDao(candidate, List.of()))
+                   .toList();
+    }
 }
--- nvi-test/src/main/java/no/sikt/nva/nvi/test/LocalDynamoTest.java
+++ nvi-test/src/main/java/no/sikt/nva/nvi/test/LocalDynamoTest.java
@@ -4,6 +4,9 @@ import static no.sikt.nva.nvi.common.DatabaseConstants.HASH_KEY;
 import static no.sikt.nva.nvi.common.DatabaseConstants.SECONDARY_INDEX_1_HASH_KEY;
 import static no.sikt.nva.nvi.common.DatabaseConstants.SECONDARY_INDEX_1_RANGE_KEY;
 import static no.sikt.nva.nvi.common.DatabaseConstants.SECONDARY_INDEX_PUBLICATION_ID;
+import static no.sikt.nva.nvi.common.DatabaseConstants.SECONDARY_INDEX_YEAR;
+import static no.sikt.nva.nvi.common.DatabaseConstants.SECONDARY_INDEX_YEAR_HASH_KEY;
+import static no.sikt.nva.nvi.common.DatabaseConstants.SECONDARY_INDEX_YEAR_RANGE_KEY;
 import static no.sikt.nva.nvi.common.DatabaseConstants.SORT_KEY;
 import static no.sikt.nva.nvi.common.utils.ApplicationConstants.NVI_TABLE_NAME;
 import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded;
@@ -29,14 +32,13 @@ import software.amazon.awssdk.services.dynamodb.model.TableStatus;
 
 public class LocalDynamoTest {
 
-    protected DynamoDbClient localDynamo;
-    private static final Long CAPACITY_DOES_NOT_MATTER = 1000L;
     public static final int SINGLE_TABLE_EXPECTED = 1;
+    private static final Long CAPACITY_DOES_NOT_MATTER = 1000L;
+    protected DynamoDbClient localDynamo;
 
     public DynamoDbClient initializeTestDatabase() {
 
         var localDynamo = DynamoDBEmbedded.create().dynamoDbClient();
-
 
         var tableName = NVI_TABLE_NAME;
         var createTableResult = createTable(localDynamo, tableName);
@@ -49,6 +51,10 @@ public class LocalDynamoTest {
         var tables = localDynamo.listTables();
         Assertions.assertEquals(SINGLE_TABLE_EXPECTED, tables.tableNames().size());
         return localDynamo;
+    }
+
+    protected ScanResponse scanDB() {
+        return localDynamo.scan(ScanRequest.builder().tableName(NVI_TABLE_NAME).build());
     }
 
     private static CreateTableResponse createTable(DynamoDbClient client, String tableName) {
@@ -65,7 +71,10 @@ public class LocalDynamoTest {
                 .globalSecondaryIndexes(
                     newGsi(SECONDARY_INDEX_PUBLICATION_ID,
                            SECONDARY_INDEX_1_HASH_KEY,
-                           SECONDARY_INDEX_1_RANGE_KEY)
+                           SECONDARY_INDEX_1_RANGE_KEY),
+                    newGsi(SECONDARY_INDEX_YEAR,
+                           SECONDARY_INDEX_YEAR_HASH_KEY,
+                           SECONDARY_INDEX_YEAR_RANGE_KEY)
                 )
                 .build();
 
@@ -87,6 +96,8 @@ public class LocalDynamoTest {
         attributeDefinitions.add(createAttributeDefinition(SORT_KEY));
         attributeDefinitions.add(createAttributeDefinition(SECONDARY_INDEX_1_HASH_KEY));
         attributeDefinitions.add(createAttributeDefinition(SECONDARY_INDEX_1_RANGE_KEY));
+        attributeDefinitions.add(createAttributeDefinition(SECONDARY_INDEX_YEAR_HASH_KEY));
+        attributeDefinitions.add(createAttributeDefinition(SECONDARY_INDEX_YEAR_RANGE_KEY));
         return attributeDefinitions;
     }
 
@@ -103,18 +114,16 @@ public class LocalDynamoTest {
                    .build();
     }
 
-    private static GlobalSecondaryIndex newGsi(String searchUsersByInstitutionIndexName,
-                                               String secondaryIndex1HashKey,
-                                               String secondaryIndex1RangeKey) {
-        ProvisionedThroughput provisionedthroughput = provisionedThroughputForLocalDatabase();
+    private static GlobalSecondaryIndex newGsi(String indexName,
+                                               String hashKey,
+                                               String rangeKey) {
+        var provisionedthroughput = provisionedThroughputForLocalDatabase();
 
         return GlobalSecondaryIndex
                    .builder()
-                   .indexName(searchUsersByInstitutionIndexName)
-                   .keySchema(
-                       KeySchemaElement.builder().attributeName(secondaryIndex1HashKey).keyType(KeyType.HASH).build(),
-                       KeySchemaElement.builder().attributeName(secondaryIndex1RangeKey).keyType(KeyType.RANGE).build()
-                   )
+                   .indexName(indexName)
+                   .keySchema(KeySchemaElement.builder().attributeName(hashKey).keyType(KeyType.HASH).build(),
+                              KeySchemaElement.builder().attributeName(rangeKey).keyType(KeyType.RANGE).build())
                    .projection(Projection.builder().projectionType(ProjectionType.ALL).build())
                    .provisionedThroughput(provisionedthroughput)
                    .build();
@@ -124,10 +133,4 @@ public class LocalDynamoTest {
         MatcherAssert.assertThat(tableKeySchema.toString(), StringContains.containsString(HASH_KEY));
         MatcherAssert.assertThat(tableKeySchema.toString(), StringContains.containsString(SORT_KEY));
     }
-
-    protected ScanResponse scanDB() {
-        return localDynamo.scan(ScanRequest.builder().tableName(NVI_TABLE_NAME).build());
-    }
-
-
 }
