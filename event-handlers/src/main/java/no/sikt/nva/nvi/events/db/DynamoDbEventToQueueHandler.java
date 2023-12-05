10162980b4b452a43c8d5186699ee4c197219bcc
monaullah <monaullah98@gmail.com>
2023-12-05 09:44:13 +0100

    Green test shouldQueueSQSEventWhenReceivingDynamoDbEvent

--- event-handlers/src/main/java/no/sikt/nva/nvi/events/db/DynamoDbEventToQueueHandler.java
+++ event-handlers/src/main/java/no/sikt/nva/nvi/events/db/DynamoDbEventToQueueHandler.java
@@ -1,8 +1,12 @@
 package no.sikt.nva.nvi.events.db;
 
+import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
+import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent.DynamodbStreamRecord;
+import java.util.List;
 import no.sikt.nva.nvi.common.queue.NviSendMessageResponse;
 import no.sikt.nva.nvi.common.queue.QueueClient;
 
@@ -16,6 +20,18 @@ public class DynamoDbEventToQueueHandler implements RequestHandler<DynamodbEvent
 
     @Override
     public Void handleRequest(DynamodbEvent input, Context context) {
+        queueClient.sendMessageBatch(mapToJsonStrings(input), "queueUrl");
         return null;
+    }
+
+    private static List<String> mapToJsonStrings(DynamodbEvent input) {
+        return input.getRecords()
+                   .stream()
+                   .map(DynamoDbEventToQueueHandler::writeAsJsonString)
+                   .toList();
+    }
+
+    private static String writeAsJsonString(DynamodbStreamRecord record) {
+        return attempt(() -> dtoObjectMapper.writeValueAsString(record)).orElseThrow();
     }
 }
