77f642ec59ba40b3ef1d8dad1a61562db98df295
monaullah <monaullah98@gmail.com>
2023-12-11 14:43:04 +0100

    shouldSendSqsMessageWithCandidateIdentifierAsMessageAttribute

--- nvi-commons/src/test/java/no/sikt/nva/nvi/common/queue/NviQueueClientTest.java
+++ nvi-commons/src/test/java/no/sikt/nva/nvi/common/queue/NviQueueClientTest.java
@@ -5,12 +5,16 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.BatchResultErrorEntry;
+import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
 import software.amazon.awssdk.services.sqs.model.SendMessageBatchRequest;
 import software.amazon.awssdk.services.sqs.model.SendMessageBatchResponse;
 import software.amazon.awssdk.services.sqs.model.SendMessageBatchResultEntry;
@@ -23,6 +27,7 @@ public class NviQueueClientTest {
     public static final String TEST_QUEUE_URL = "url";
     public static final String TEST_MESSAGE_ID = "some_test_id";
     public static final String MESSAGE_FAILED_ID = "some_failed_id";
+    public static final String MESSAGE_ATTRIBUTE_CANDIDATE_IDENTIFIER = "candidateIdentifier";
 
     @Test
     void shouldSendSqsMessageAndReturnId() {
@@ -32,6 +37,19 @@ public class NviQueueClientTest {
         var client = new NviQueueClient(sqsClient);
 
         var result = client.sendMessage(TEST_PAYLOAD, TEST_QUEUE_URL);
+
+        assertThat(result.messageId(), is(equalTo(TEST_MESSAGE_ID)));
+    }
+
+    @Test
+    void shouldSendSqsMessageWithCandidateIdentifierAsMessageAttribute() {
+        var sqsClient = mock(SqsClient.class);
+        var candidateIdentifier = UUID.randomUUID();
+        when(sqsClient.sendMessage(eq(expectedSendMessageRequest(candidateIdentifier)))).thenReturn(
+            SendMessageResponse.builder().messageId(TEST_MESSAGE_ID).build());
+        var client = new NviQueueClient(sqsClient);
+
+        var result = client.sendMessage(TEST_PAYLOAD, TEST_QUEUE_URL, candidateIdentifier);
 
         assertThat(result.messageId(), is(equalTo(TEST_MESSAGE_ID)));
     }
@@ -49,5 +67,15 @@ public class NviQueueClientTest {
         var result = client.sendMessageBatch(List.of(TEST_PAYLOAD), TEST_QUEUE_URL);
         assertThat(result.successful(), containsInAnyOrder(TEST_MESSAGE_ID));
         assertThat(result.failed(), containsInAnyOrder(MESSAGE_FAILED_ID));
+    }
+
+    private static SendMessageRequest expectedSendMessageRequest(UUID candidateIdentifier) {
+        return SendMessageRequest.builder()
+                   .messageBody(TEST_PAYLOAD)
+                   .messageAttributes(Map.of(MESSAGE_ATTRIBUTE_CANDIDATE_IDENTIFIER,
+                                             MessageAttributeValue.builder()
+                                                 .stringValue(candidateIdentifier.toString())
+                                                 .build()))
+                   .queueUrl(TEST_QUEUE_URL).build();
     }
 }
