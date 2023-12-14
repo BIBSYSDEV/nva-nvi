093e7d87fe427848b3c6776ebb06d03c352c357a
monaullah <monaullah98@gmail.com>
2023-12-14 15:30:48 +0100

    Refactor

--- event-handlers/src/main/java/no/sikt/nva/nvi/events/db/TopicConstants.java
+++ event-handlers/src/main/java/no/sikt/nva/nvi/events/db/TopicConstants.java
@@ -2,11 +2,11 @@ package no.sikt.nva.nvi.events.db;
 
 public final class TopicConstants {
 
-    protected static final String CANDIDATE_INSERT_TOPIC = "Candidate.Insert";
-    protected static final String CANDIDATE_UPDATE_APPLICABLE_TOPIC = "Candidate.Update.Applicable";
-    protected static final String CANDIDATE_UPDATE_NOT_APPLICABLE_TOPIC = "Candidate.Update.NotApplicable";
-    protected static final String CANDIDATE_REMOVED_TOPIC = "Candidate.Removed";
-    protected static final String APPROVAL_INSERT_TOPIC = "Approval.Insert";
-    protected static final String APPROVAL_UPDATE_TOPIC = "Approval.Update";
-    protected static final String APPROVAL_REMOVE_TOPIC = "Approval.Remove";
+    static final String CANDIDATE_INSERT_TOPIC = "Candidate.Insert";
+    static final String CANDIDATE_UPDATE_APPLICABLE_TOPIC = "Candidate.Update.Applicable";
+    static final String CANDIDATE_UPDATE_NOT_APPLICABLE_TOPIC = "Candidate.Update.NotApplicable";
+    static final String CANDIDATE_REMOVED_TOPIC = "Candidate.Removed";
+    static final String APPROVAL_INSERT_TOPIC = "Approval.Insert";
+    static final String APPROVAL_UPDATE_TOPIC = "Approval.Update";
+    static final String APPROVAL_REMOVE_TOPIC = "Approval.Remove";
 }
