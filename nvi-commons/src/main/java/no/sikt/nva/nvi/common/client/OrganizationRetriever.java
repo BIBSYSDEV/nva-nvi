dfa413769ee5d308d6363bbd5922b7954038cd19
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2024-01-08 10:49:22 +0100

    Use cristin proxy version=2023-05-26 (#219)

--- nvi-commons/src/main/java/no/sikt/nva/nvi/common/client/OrganizationRetriever.java
+++ nvi-commons/src/main/java/no/sikt/nva/nvi/common/client/OrganizationRetriever.java
@@ -14,7 +14,9 @@ import org.slf4j.LoggerFactory;
 public class OrganizationRetriever {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OrganizationRetriever.class);
-    private static final String CONTENT_TYPE = "application/json";
+    private static final String APPLICATION_JSON = "application/json";
+    private static final String CRISTIN_VERSION = "; version=2023-05-26";
+    private static final String MEDIA_TYPE_JSON_V2 = APPLICATION_JSON + CRISTIN_VERSION;
     private static final String COULD_NOT_FETCH_CRISTIN_ORG_MESSAGE = "Could not fetch Cristin organization for: ";
     private static final String ERROR_COULD_NOT_FETCH_CRISTIN_ORG = COULD_NOT_FETCH_CRISTIN_ORG_MESSAGE + "{}. "
                                                                     + "Response code: {}";
@@ -40,7 +42,7 @@ public class OrganizationRetriever {
     }
 
     private HttpResponse<String> getResponse(URI uri) {
-        return Optional.ofNullable(uriRetriever.fetchResponse(uri, CONTENT_TYPE))
+        return Optional.ofNullable(uriRetriever.fetchResponse(uri, MEDIA_TYPE_JSON_V2))
                    .stream()
                    .filter(Optional::isPresent)
                    .map(Optional::get)
