8730314c9162324a408a92b898b2085dde061b23
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2024-01-29 09:36:49 +0100

    Attempting to migrate instance type (#237)

--- nvi-commons/src/main/java/no/sikt/nva/nvi/common/db/CandidateDao.java
+++ nvi-commons/src/main/java/no/sikt/nva/nvi/common/db/CandidateDao.java
@@ -283,6 +283,20 @@ public final class CandidateDao extends Dao {
                               BigDecimal totalPoints
     ) {
 
+        @Deprecated
+        //TODO: Should be removed once we have migrated instanceType to String
+        public String instanceType() {
+            var enums = Arrays.stream(InstanceType.values()).toList();
+            var instanceTypeEnum = enums.stream()
+                                       .filter(value -> value.toString().equals(instanceType))
+                                       .findFirst();
+            if (instanceTypeEnum.isPresent()) {
+                return instanceTypeEnum.get().getValue();
+            } else {
+                return instanceType;
+            }
+        }
+
         public static Builder builder() {
             return new Builder();
         }
