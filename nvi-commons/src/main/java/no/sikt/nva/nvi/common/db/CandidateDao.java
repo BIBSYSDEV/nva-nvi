ab4afda171d041943f54034f98ce99763a66bf7b
monaullah <monaullah98@gmail.com>
2024-04-05 15:28:53 +0200

    Add migration code in getter

--- nvi-commons/src/main/java/no/sikt/nva/nvi/common/db/CandidateDao.java
+++ nvi-commons/src/main/java/no/sikt/nva/nvi/common/db/CandidateDao.java
@@ -182,7 +182,16 @@ public final class CandidateDao extends Dao {
 
     @DynamoDbAttribute(PERIOD_YEAR_FIELD)
     public String periodYear() {
-        return periodYear;
+        return migratePeriodYear();
+    }
+
+    @Deprecated
+    private String migratePeriodYear() {
+        return isApplicableAndMissingPeriodYear() ? candidate.publicationDate().year() : periodYear;
+    }
+
+    private boolean isApplicableAndMissingPeriodYear() {
+        return isNull(periodYear) && candidate.applicable();
     }
 
     public enum DbLevel {
