035be233eb8b7e9ce2a9e0cfba207b6eb9e58b71
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2024-04-12 14:19:32 +0200

    mapping creator affiliation points (#292)

---
+++ event-handlers/src/main/java/no/sikt/nva/nvi/events/cristin/InstitutionPoints.java
@@ -0,0 +1,12 @@
+package no.sikt.nva.nvi.events.cristin;
+
+import java.math.BigDecimal;
+import java.net.URI;
+import java.util.List;
+
+public record InstitutionPoints(URI institutionId, BigDecimal points, List<CreatorPoints> creatorPoints) {
+
+    public record CreatorPoints(URI creatorId, URI affiliationId, BigDecimal points) {
+
+    }
+}
