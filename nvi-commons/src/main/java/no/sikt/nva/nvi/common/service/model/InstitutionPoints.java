2aa43dbf4793794b18e3a84a5886b81d5845ca9a
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2024-04-18 15:22:21 +0200

    adding nonNull check for institutionPoints (#300)

--- nvi-commons/src/main/java/no/sikt/nva/nvi/common/service/model/InstitutionPoints.java
+++ nvi-commons/src/main/java/no/sikt/nva/nvi/common/service/model/InstitutionPoints.java
@@ -1,21 +1,26 @@
 package no.sikt.nva.nvi.common.service.model;
 
+import static java.util.Objects.nonNull;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.List;
 import no.sikt.nva.nvi.common.db.CandidateDao.DbInstitutionPoints;
 import no.sikt.nva.nvi.common.db.CandidateDao.DbInstitutionPoints.DbCreatorAffiliationPoints;
 
-public record InstitutionPoints(URI institutionId,
-                                BigDecimal institutionPoints,
+public record InstitutionPoints(URI institutionId, BigDecimal institutionPoints,
                                 List<CreatorAffiliationPoints> creatorAffiliationPoints) {
 
     public static InstitutionPoints from(DbInstitutionPoints dbInstitutionPoints) {
-        return new InstitutionPoints(dbInstitutionPoints.institutionId(),
-                                     dbInstitutionPoints.points(),
-                                     dbInstitutionPoints.creatorAffiliationPoints().stream()
+        return new InstitutionPoints(dbInstitutionPoints.institutionId(), dbInstitutionPoints.points(),
+                                     dbInstitutionPoints.creatorAffiliationPoints()
+                                         .stream()
                                          .map(CreatorAffiliationPoints::from)
                                          .toList());
+    }
+
+    @Override
+    public List<CreatorAffiliationPoints> creatorAffiliationPoints() {
+        return nonNull(creatorAffiliationPoints) ? creatorAffiliationPoints : List.of();
     }
 
     public record CreatorAffiliationPoints(URI nviCreator, URI affiliationId, BigDecimal points) {
