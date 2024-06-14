fcc492d2ff3fb75d42ac92796896e22114c8589d
monaullah <monaullah98@gmail.com>
2024-06-14 13:30:03 +0200

    Approval, add json property institutionIdentifier

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/model/document/Approval.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/model/document/Approval.java
@@ -1,6 +1,7 @@
 package no.sikt.nva.nvi.index.model.document;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
@@ -8,6 +9,7 @@ import java.net.URI;
 import java.util.Map;
 import java.util.Set;
 import no.sikt.nva.nvi.common.service.model.GlobalApprovalStatus;
+import nva.commons.core.paths.UriWrapper;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonSerialize
@@ -23,8 +25,15 @@ public record Approval(URI institutionId,
                        String assignee,
                        GlobalApprovalStatus globalApprovalStatus) {
 
+    private static final String INSTITUTION_IDENTIFIER = "institutionIdentifier";
+
     public static Builder builder() {
         return new Builder();
+    }
+
+    @JsonProperty(INSTITUTION_IDENTIFIER)
+    public String getInstitutionIdentifier() {
+        return UriWrapper.fromUri(institutionId).getLastPathElement();
     }
 
     public static final class Builder {
