1174eb35d09bf41287453634cd802a4126675c3b
monaullah <monaullah98@gmail.com>
2024-06-26 09:08:54 +0200

    FetchNviCandidateHandler, validate viewing scope

--- nvi-rest/src/main/java/no/sikt/nva/nvi/rest/fetch/FetchNviCandidateHandler.java
+++ nvi-rest/src/main/java/no/sikt/nva/nvi/rest/fetch/FetchNviCandidateHandler.java
@@ -4,14 +4,18 @@ import static java.net.HttpURLConnection.HTTP_OK;
 import static no.sikt.nva.nvi.common.db.DynamoRepository.defaultDynamoClient;
 import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
-import java.net.URI;
 import java.util.UUID;
+import no.sikt.nva.nvi.common.client.OrganizationRetriever;
 import no.sikt.nva.nvi.common.db.CandidateRepository;
 import no.sikt.nva.nvi.common.db.PeriodRepository;
 import no.sikt.nva.nvi.common.service.dto.CandidateDto;
 import no.sikt.nva.nvi.common.service.exception.CandidateNotFoundException;
 import no.sikt.nva.nvi.common.service.model.Candidate;
 import no.sikt.nva.nvi.common.utils.ExceptionMapper;
+import no.sikt.nva.nvi.common.validator.ViewingScopeValidator;
+import no.sikt.nva.nvi.common.validator.ViewingScopeValidatorImpl;
+import no.unit.nva.auth.uriretriever.UriRetriever;
+import no.unit.nva.clients.IdentityServiceClient;
 import nva.commons.apigateway.AccessRight;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
@@ -24,16 +28,20 @@ public class FetchNviCandidateHandler extends ApiGatewayHandler<Void, CandidateD
     public static final String CANDIDATE_IDENTIFIER = "candidateIdentifier";
     private final CandidateRepository candidateRepository;
     private final PeriodRepository periodRepository;
+    private final ViewingScopeValidator viewingScopeValidator;
 
     @JacocoGenerated
     public FetchNviCandidateHandler() {
-        this(new CandidateRepository(defaultDynamoClient()), new PeriodRepository(defaultDynamoClient()));
+        this(new CandidateRepository(defaultDynamoClient()), new PeriodRepository(defaultDynamoClient()),
+             defaultViewingScopeValidator());
     }
 
-    public FetchNviCandidateHandler(CandidateRepository candidateRepository, PeriodRepository periodRepository) {
+    public FetchNviCandidateHandler(CandidateRepository candidateRepository, PeriodRepository periodRepository,
+                                    ViewingScopeValidator viewingScopeValidator) {
         super(Void.class);
         this.candidateRepository = candidateRepository;
         this.periodRepository = periodRepository;
+        this.viewingScopeValidator = viewingScopeValidator;
     }
 
     @Override
@@ -44,11 +52,10 @@ public class FetchNviCandidateHandler extends ApiGatewayHandler<Void, CandidateD
     @Override
     protected CandidateDto processInput(Void input, RequestInfo requestInfo, Context context)
         throws ApiGatewayException {
-        var topLevelCristinOrg = requestInfo.getTopLevelOrgCristinId().orElseThrow();
         return attempt(() -> requestInfo.getPathParameter(CANDIDATE_IDENTIFIER)).map(UUID::fromString)
                    .map(identifier -> Candidate.fetch(() -> identifier, candidateRepository, periodRepository))
                    .map(this::checkIfApplicable)
-                   .map(candidate -> validateTopLevelOrg(candidate, topLevelCristinOrg))
+                   .map(candidate -> validateViewingScope(candidate, requestInfo))
                    .map(Candidate::toDto)
                    .orElseThrow(ExceptionMapper::map);
     }
@@ -73,16 +80,21 @@ public class FetchNviCandidateHandler extends ApiGatewayHandler<Void, CandidateD
         return requestInfo.userIsAuthorized(AccessRight.MANAGE_NVI_CANDIDATES);
     }
 
-    private static Candidate validateTopLevelOrg(Candidate candidate, URI topLevelOrgCristinId)
-        throws UnauthorizedException {
-        if (doesNotContainApprovalForInstitution(candidate, topLevelOrgCristinId)) {
+    @JacocoGenerated
+    private static ViewingScopeValidatorImpl defaultViewingScopeValidator() {
+        return new ViewingScopeValidatorImpl(IdentityServiceClient.prepare(),
+                                             new OrganizationRetriever(new UriRetriever()));
+    }
+
+    private Candidate validateViewingScope(Candidate candidate, RequestInfo requestInfo) throws UnauthorizedException {
+        if (isNotAllowedToAccessOneOfOrganizations(requestInfo.getUserName(), candidate)) {
             throw new UnauthorizedException();
         }
         return candidate;
     }
 
-    private static boolean doesNotContainApprovalForInstitution(Candidate candidate, URI topLevelOrgCristinId) {
-        return candidate.getApprovals().keySet().stream().noneMatch(uri -> uri.equals(topLevelOrgCristinId));
+    private boolean isNotAllowedToAccessOneOfOrganizations(String userName, Candidate candidate) {
+        return !viewingScopeValidator.userIsAllowedToAccessOneOf(userName, candidate.getNviCreatorAffiliations());
     }
 
     private Candidate checkIfApplicable(Candidate candidate) {
