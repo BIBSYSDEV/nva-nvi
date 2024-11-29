6a63ebfe9e3d62c1779a0c7f62d2f7ec2aa73e38
monaullah <monaullah98@gmail.com>
2024-11-29 11:09:20 +0100

    Add isAssigned check

--- event-handlers/src/main/java/no/sikt/nva/nvi/events/evaluator/PointService.java
+++ event-handlers/src/main/java/no/sikt/nva/nvi/events/evaluator/PointService.java
@@ -36,11 +36,12 @@ import no.sikt.nva.nvi.events.evaluator.model.VerifiedNviCreator;
 
 public final class PointService {
 
+    public static final String TYPE_JOURNAL = "Journal";
     private static final String COUNTRY_CODE_NORWAY = "NO";
     private static final String ROLE_CREATOR = "Creator";
     private static final String TYPE = "type";
     private static final String TYPE_SERIES = "Series";
-    public static final String TYPE_JOURNAL = "Journal";
+    private static final String UNASSIGNED = "Unassigned";
     private final OrganizationRetriever organizationRetriever;
 
     public PointService(OrganizationRetriever organizationRetriever) {
@@ -140,11 +141,16 @@ public final class PointService {
     }
 
     private static String extractAcademicChapterChannel(JsonNode jsonNode) {
-        if (nonNull(extractJsonNodeTextValue(jsonNode, JSON_PTR_CHAPTER_SERIES_SCIENTIFIC_VALUE))) {
+        if (nonNull(extractJsonNodeTextValue(jsonNode, JSON_PTR_CHAPTER_SERIES_SCIENTIFIC_VALUE))
+            && isAssigned(extractJsonNodeTextValue(jsonNode, JSON_PTR_CHAPTER_SERIES_SCIENTIFIC_VALUE))) {
             return jsonNode.at(JSON_PTR_CHAPTER_SERIES).toString();
         } else {
             return jsonNode.at(JSON_PTR_CHAPTER_PUBLISHER).toString();
         }
+    }
+
+    private static boolean isAssigned(String scientificValue) {
+        return !UNASSIGNED.equals(scientificValue);
     }
 
     private static String extractBookChannel(JsonNode jsonNode) {
