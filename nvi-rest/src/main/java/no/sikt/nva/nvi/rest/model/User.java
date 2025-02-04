ae9251de0216a2d7f6a57a77bfc66dc2575ddbce
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-02-04 12:46:24 +0100

    refactor: Delete unused code

--- nvi-rest/src/main/java/no/sikt/nva/nvi/rest/model/User.java
+++
@@ -1,9 +0,0 @@
-package no.sikt.nva.nvi.rest.model;
-
-import java.util.List;
-import nva.commons.apigateway.AccessRight;
-
-public record User(List<Role> roles) {
-
-  public record Role(List<AccessRight> accessRights) {}
-}
