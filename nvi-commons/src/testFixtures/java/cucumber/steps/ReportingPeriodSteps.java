ca0b830a40bdb9605c845b537f1677989e49d909
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-04-25 10:27:11 +0200

    build: Upgrade dependencies (#481)
    Changes:
    - Upgrades `nva-commons` to `2.2.4`
    - Upgrades minor versions of various dependencies
    - Upgrades Gradle version to `8.13`
    - Adds a Gradle plugin to detect outdated dependencies (can be run with
    the Gradle task `dependencyUpdates`, under `Help`)
    - Removes unused dependency `cucumber-junit` (this is for JUnit4, we use
    `cucumber-junit-platform-engine` for JUnit5) in order to remove JUnit4
    from the project entirely.

--- nvi-commons/src/testFixtures/java/cucumber/steps/ReportingPeriodSteps.java
+++ nvi-commons/src/testFixtures/java/cucumber/steps/ReportingPeriodSteps.java
@@ -2,8 +2,8 @@ package cucumber.steps;
 
 import static no.sikt.nva.nvi.common.db.PeriodRepositoryFixtures.updatePeriod;
 import static no.sikt.nva.nvi.common.db.PeriodRepositoryFixtures.upsertPeriod;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import cucumber.contexts.ScenarioContext;
 import io.cucumber.java.en.Given;
