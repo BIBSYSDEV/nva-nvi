a4f0d5f1bccc140f9a94ad67a80cfb978f167973
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-06-16 14:11:09 +0200

    refactor: Use value class with validation for username

--- nvi-commons/src/main/java/no/sikt/nva/nvi/common/model/UserInstance.java
+++ nvi-commons/src/main/java/no/sikt/nva/nvi/common/model/UserInstance.java
@@ -1,16 +1,19 @@
 package no.sikt.nva.nvi.common.model;
 
+import static no.sikt.nva.nvi.common.service.model.Username.fromString;
+
 import java.net.URI;
 import java.util.Collection;
+import no.sikt.nva.nvi.common.service.model.Username;
 import nva.commons.apigateway.AccessRight;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.UnauthorizedException;
 
 public record UserInstance(
-    String userName, URI topLevelOrganizationId, Collection<AccessRight> accessRights) {
+    Username userName, URI topLevelOrganizationId, Collection<AccessRight> accessRights) {
 
   public static UserInstance fromRequestInfo(RequestInfo requestInfo) throws UnauthorizedException {
-    var userName = requestInfo.getUserName();
+    var userName = fromString(requestInfo.getUserName());
     var topLevelOrganization = requestInfo.getTopLevelOrgCristinId().orElse(null);
     var accessRights = requestInfo.getAccessRights();
     return new UserInstance(userName, topLevelOrganization, accessRights);
