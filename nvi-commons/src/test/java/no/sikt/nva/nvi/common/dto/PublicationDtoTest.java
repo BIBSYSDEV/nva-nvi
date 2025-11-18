adcb3a82dbae09f7d23c64000fd832e17e4eac3a
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2025-11-18 14:06:31 +0100

    validating isbnList presence for AcademicMonograph and AcademicCommentary (#566)
    - Validating isbnList presence for AcademicMonograph and
    AcademicCommentary

--- nvi-commons/src/test/java/no/sikt/nva/nvi/common/dto/PublicationDtoTest.java
+++ nvi-commons/src/test/java/no/sikt/nva/nvi/common/dto/PublicationDtoTest.java
@@ -1,6 +1,5 @@
 package no.sikt.nva.nvi.common.dto;
 
-import static no.sikt.nva.nvi.common.model.InstanceType.ACADEMIC_CHAPTER;
 import static no.sikt.nva.nvi.test.TestUtils.randomYear;
 import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
@@ -10,41 +9,57 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 import no.sikt.nva.nvi.common.exceptions.ValidationException;
-import org.junit.jupiter.api.Test;
+import no.sikt.nva.nvi.common.model.InstanceType;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PublicationDtoTest {
 
-  @Test
-  void shouldThrowValidationExceptionWhenPublicationIsAcademicChapterAndIsMissingIsbn() {
-    var publication = createPublicationDto(Collections.emptyList());
+  public static Stream<Arguments> isbnRequiringTypeProvider() {
+    return Stream.of(Arguments.of("AcademicChapter", "AcademicMonograph", "AcademicCommentary"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("isbnRequiringTypeProvider")
+  void shouldThrowValidationExceptionWhenPublicationHasInstanceTypeAndIsMissingIsbn(
+      String instanceType) {
+    var publication =
+        createPublicationDto(Collections.emptyList(), InstanceType.parse(instanceType));
 
     Executable executable = publication::validate;
     var exception = assertThrows(ValidationException.class, executable);
 
     assertEquals(
-        "Required field 'isbnList' must not be empty for AcademicChapter", exception.getMessage());
+        "Required field 'isbnList' must not be empty for %s".formatted(instanceType),
+        exception.getMessage());
   }
 
-  @Test
+  @ParameterizedTest
+  @MethodSource("isbnRequiringTypeProvider")
   void
-      shouldThrowValidationExceptionWhenPublicationIsAcademicChapterAndHasIsbnListWithNullValuesOnly() {
-    var publication = createPublicationDto(Arrays.asList(null, null));
+      shouldThrowValidationExceptionWhenPublicationHasInstanceTypeTypeAndHasIsbnListWithNullValuesOnly(
+          String instanceType) {
+    var publication =
+        createPublicationDto(Arrays.asList(null, null), InstanceType.parse(instanceType));
 
     Executable executable = publication::validate;
     var exception = assertThrows(ValidationException.class, executable);
 
     assertEquals(
-        "Required field 'isbnList' must not be empty for AcademicChapter", exception.getMessage());
+        "Required field 'isbnList' must not be empty for %s".formatted(instanceType),
+        exception.getMessage());
   }
 
-  private PublicationDto createPublicationDto(List<String> isbnList) {
+  private PublicationDto createPublicationDto(List<String> isbnList, InstanceType instanceType) {
     return PublicationDto.builder()
         .withId(randomUri())
         .withStatus(randomString())
         .withPublicationDate(new PublicationDateDto(randomYear(), null, null))
-        .withPublicationType(ACADEMIC_CHAPTER)
+        .withPublicationType(instanceType)
         .withPublicationChannels(Collections.emptyList())
         .withTopLevelOrganizations(Collections.emptyList())
         .withContributors(Collections.emptyList())
