3838fa6ff78cd92b425431596093c4940e84dcf7
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-11-25 12:30:09 +0100

    docs: Update schema

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/InstitutionStatusAggregationReport.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/InstitutionStatusAggregationReport.java
@@ -1,8 +1,10 @@
 package no.sikt.nva.nvi.index.model.report;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 import java.net.URI;
 import java.util.Map;
 
-public record InstitutionStatusAggregationReport(@JsonValue Map<URI, OrganizationStatusAggregation> organizations) {}
+public record InstitutionStatusAggregationReport(
+    String year,
+    URI topLevelOrganizationId,
+    OrganizationStatusAggregation totals,
+    Map<URI, OrganizationStatusAggregation> byOrganization) {}
