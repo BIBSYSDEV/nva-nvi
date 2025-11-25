5175b00fb8218c1d3decb127c56b9a7939de5df0
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-11-25 11:18:50 +0100

    docs: Update schema

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/OrganizationStatusAggregation.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/OrganizationStatusAggregation.java
@@ -7,7 +7,7 @@ import no.sikt.nva.nvi.index.model.document.ApprovalStatus;
 
 public record OrganizationStatusAggregation(
     int candidateCount,
-    BigDecimal pointsFromAffiliatedCreators,
+    BigDecimal points,
     Map<GlobalApprovalStatus, Integer> globalApprovalStatus,
     Map<ApprovalStatus, Integer> approvalStatus) {
 
@@ -15,6 +15,6 @@ public record OrganizationStatusAggregation(
     public OrganizationStatusAggregation {
         // TODO: Add helper method to copy the two approval status maps and ensure we have values for all enums
         // TODO Add validation that candidateCount is greater than zero
-        // TODO Add validation that pointsFromAffiliatedCreators is greater than zero
+        // TODO Add validation that points is greater than zero
     }
 }
