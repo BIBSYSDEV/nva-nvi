5fe568a7143bca42c6aaa58487fe8a418f4c30fe
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-11-25 12:34:03 +0100

    wip:

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/InstitutionStatusAggregationReport.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/InstitutionStatusAggregationReport.java
@@ -1,8 +1,12 @@
 package no.sikt.nva.nvi.index.model.report;
 
+import nva.commons.core.JacocoGenerated;
+
 import java.net.URI;
 import java.util.Map;
 
+// TODO: Not in use yet, intended for NP-50248
+@JacocoGenerated
 public record InstitutionStatusAggregationReport(
     String year,
     URI topLevelOrganizationId,
--- index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/OrganizationStatusAggregation.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/OrganizationStatusAggregation.java
@@ -11,10 +11,10 @@ public record OrganizationStatusAggregation(
     Map<GlobalApprovalStatus, Integer> globalApprovalStatus,
     Map<ApprovalStatus, Integer> approvalStatus) {
 
-
-    public OrganizationStatusAggregation {
-        // TODO: Add helper method to copy the two approval status maps and ensure we have values for all enums
-        // TODO Add validation that candidateCount is greater than zero
-        // TODO Add validation that points is greater than zero
-    }
+  public OrganizationStatusAggregation {
+    // TODO: Add helper method to copy the two approval status maps and ensure we have values for
+    // all enums
+    // TODO Add validation that candidateCount is greater than zero
+    // TODO Add validation that points is greater than zero
+  }
 }
