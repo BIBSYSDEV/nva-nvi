20c7624fbde54e95b0802a263be074c2209d6600
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-12-03 07:51:46 +0100

    wip: Add ignore annotation

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/InstitutionStatusAggregationReport.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/InstitutionStatusAggregationReport.java
@@ -2,7 +2,10 @@ package no.sikt.nva.nvi.index.model.report;
 
 import java.net.URI;
 import java.util.Map;
+import nva.commons.core.JacocoGenerated;
 
+// TODO: Not in use yet, intended for NP-50248
+@JacocoGenerated
 public record InstitutionStatusAggregationReport(
     String year,
     URI topLevelOrganizationId,
