869f538a3d762968de68a87ed48a668a0b477fa4
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-12-03 07:48:39 +0100

    refactor: Add distinct types

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/InstitutionStatusAggregationReport.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/InstitutionStatusAggregationReport.java
@@ -2,12 +2,9 @@ package no.sikt.nva.nvi.index.model.report;
 
 import java.net.URI;
 import java.util.Map;
-import nva.commons.core.JacocoGenerated;
 
-// TODO: Not in use yet, intended for NP-50248
-@JacocoGenerated
 public record InstitutionStatusAggregationReport(
     String year,
     URI topLevelOrganizationId,
-    OrganizationStatusAggregation totals,
-    Map<URI, OrganizationStatusAggregation> byOrganization) {}
+    TopLevelAggregation totals,
+    Map<URI, DirectAffiliationAggregation> byOrganization) {}
