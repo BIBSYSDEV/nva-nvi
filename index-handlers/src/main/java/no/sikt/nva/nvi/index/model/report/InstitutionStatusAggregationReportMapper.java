1d2778cf271ab9b1711235dd58f2e64cf14afbeb
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-12-04 16:10:37 +0100

    refactor: QA

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/InstitutionStatusAggregationReportMapper.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/InstitutionStatusAggregationReportMapper.java
@@ -1,5 +1,6 @@
 package no.sikt.nva.nvi.index.model.report;
 
+import static java.util.stream.Collectors.toMap;
 import static no.sikt.nva.nvi.index.query.InstitutionStatusAggregation.AFFILIATION_AGGREGATE;
 import static no.sikt.nva.nvi.index.query.InstitutionStatusAggregation.BY_ORGANIZATION;
 import static no.sikt.nva.nvi.index.query.InstitutionStatusAggregation.FILTERED_BY;
@@ -12,7 +13,6 @@ import static no.sikt.nva.nvi.index.utils.SearchConstants.POINTS;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
 import no.sikt.nva.nvi.common.service.model.GlobalApprovalStatus;
 import no.sikt.nva.nvi.index.model.document.ApprovalStatus;
@@ -44,7 +44,7 @@ public final class InstitutionStatusAggregationReportMapper {
   }
 
   private static Map<URI, DirectAffiliationAggregation> extractByOrganization(Aggregate aggregate) {
-    var summariesByOrganization =
+    var organizationBuckets =
         aggregate
             .nested()
             .aggregations()
@@ -55,15 +55,22 @@ public final class InstitutionStatusAggregationReportMapper {
             .nested()
             .aggregations()
             .get(BY_ORGANIZATION)
-            .sterms();
+            .sterms()
+            .buckets()
+            .array();
 
-    var result = new HashMap<URI, DirectAffiliationAggregation>();
-    for (var bucket : summariesByOrganization.buckets().array()) {
-      var organizationId = URI.create(bucket.key());
-      var organizationSummary = extractDirectAffiliationAggregation(bucket);
-      result.put(organizationId, organizationSummary);
-    }
-    return result;
+    var aggregationMap =
+        organizationBuckets.stream()
+            .collect(
+                toMap(
+                    InstitutionStatusAggregationReportMapper::uriFromKey,
+                    InstitutionStatusAggregationReportMapper::extractDirectAffiliationAggregation));
+
+    return Map.copyOf(aggregationMap);
+  }
+
+  private static URI uriFromKey(StringTermsBucket bucket) {
+    return URI.create(bucket.key());
   }
 
   private static DirectAffiliationAggregation extractDirectAffiliationAggregation(
@@ -85,40 +92,32 @@ public final class InstitutionStatusAggregationReportMapper {
 
   private static Map<GlobalApprovalStatus, Integer> extractGlobalApprovalStatusCounts(
       Map<String, Aggregate> aggregations) {
-    var globalStatusAggregate = aggregations.get(GLOBAL_APPROVAL_STATUS).sterms();
-    var result = initializeGlobalApprovalStatusMap();
+    var buckets = aggregations.get(GLOBAL_APPROVAL_STATUS).sterms().buckets().array();
+    var result = enumCounterMap(GlobalApprovalStatus.class);
 
-    for (var bucket : globalStatusAggregate.buckets().array()) {
+    for (var bucket : buckets) {
       var status = GlobalApprovalStatus.parse(bucket.key());
       result.put(status, (int) bucket.docCount());
     }
-    return result;
+    return Map.copyOf(result);
   }
 
   private static Map<ApprovalStatus, Integer> extractApprovalStatusCounts(
       Map<String, Aggregate> aggregations) {
-    var statusAggregate = aggregations.get(APPROVAL_STATUS).sterms();
-    var result = initializeApprovalStatusMap();
+    var buckets = aggregations.get(APPROVAL_STATUS).sterms().buckets().array();
+    var result = enumCounterMap(ApprovalStatus.class);
 
-    for (var bucket : statusAggregate.buckets().array()) {
+    for (var bucket : buckets) {
       var status = ApprovalStatus.parse(bucket.key());
       result.put(status, (int) bucket.docCount());
     }
-    return result;
+    return Map.copyOf(result);
   }
 
-  private static Map<GlobalApprovalStatus, Integer> initializeGlobalApprovalStatusMap() {
-    var map = new EnumMap<GlobalApprovalStatus, Integer>(GlobalApprovalStatus.class);
-    for (var status : GlobalApprovalStatus.values()) {
-      map.put(status, INITIAL_STATUS_COUNT);
-    }
-    return map;
-  }
-
-  private static Map<ApprovalStatus, Integer> initializeApprovalStatusMap() {
-    var map = new EnumMap<ApprovalStatus, Integer>(ApprovalStatus.class);
-    for (var status : ApprovalStatus.values()) {
-      map.put(status, INITIAL_STATUS_COUNT);
+  private static <E extends Enum<E>> EnumMap<E, Integer> enumCounterMap(Class<E> enumClass) {
+    var map = new EnumMap<E, Integer>(enumClass);
+    for (var value : enumClass.getEnumConstants()) {
+      map.put(value, INITIAL_STATUS_COUNT);
     }
     return map;
   }
