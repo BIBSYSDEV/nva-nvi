9b42f7f73470ab9670e182a8f27135f39dc0aa26
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-12-05 11:06:16 +0100

    fix: Add temporary alias

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/model/document/OrganizationSummary.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/model/document/OrganizationSummary.java
@@ -1,5 +1,6 @@
 package no.sikt.nva.nvi.index.model.document;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import java.math.BigDecimal;
 import java.net.URI;
 import no.sikt.nva.nvi.common.service.model.GlobalApprovalStatus;
@@ -7,6 +8,7 @@ import no.sikt.nva.nvi.common.service.model.GlobalApprovalStatus;
 /** Denormalized model to support aggregate queries by organization. */
 public record OrganizationSummary(
     URI organizationId,
-    BigDecimal points,
+    // FIXME: Temporary alias for renamed field. Remove after full reindexing is completed.
+    @JsonAlias("directPoints") BigDecimal points,
     ApprovalStatus approvalStatus,
     GlobalApprovalStatus globalApprovalStatus) {}
