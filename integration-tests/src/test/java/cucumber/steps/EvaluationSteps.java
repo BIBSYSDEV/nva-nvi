073fc2fac5c4ff521102a9da7f6cc03746c10017
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-12-08 10:47:17 +0100

    [np-50226] fix: Validate upsert based on target period (#582)
    Ticket: https://sikt.atlassian.net/browse/NP-50226
    
    Changes:
    
    - Adds more structured and extensive test cases for evaluation based on
    reporting period
    - Fixes bug in period validation that prevented changing publication
    year in some cases

--- integration-tests/src/test/java/cucumber/steps/EvaluationSteps.java
+++ integration-tests/src/test/java/cucumber/steps/EvaluationSteps.java
@@ -1,12 +1,15 @@
 package cucumber.steps;
 
+import static java.util.Collections.emptyList;
 import static no.sikt.nva.nvi.common.db.PeriodRepositoryFixtures.setupClosedPeriod;
 import static no.sikt.nva.nvi.common.db.PeriodRepositoryFixtures.setupFuturePeriod;
 import static no.sikt.nva.nvi.common.db.PeriodRepositoryFixtures.setupOpenPeriod;
 import static no.sikt.nva.nvi.common.model.ContributorFixtures.unverifiedCreatorFrom;
 import static no.sikt.nva.nvi.common.model.ContributorFixtures.verifiedCreatorFrom;
-import static no.sikt.nva.nvi.common.model.PublicationDateFixtures.randomPublicationDate;
+import static no.sikt.nva.nvi.common.model.PublicationDateFixtures.randomPublicationDateInYear;
 import static no.sikt.nva.nvi.test.TestConstants.COUNTRY_CODE_NORWAY;
+import static no.sikt.nva.nvi.test.TestUtils.CURRENT_YEAR;
+import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -15,10 +18,14 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.net.URI;
+import java.time.Instant;
 import no.sikt.nva.nvi.common.SampleExpandedPublicationFactory;
 import no.sikt.nva.nvi.common.TestScenario;
+import no.sikt.nva.nvi.common.db.ReportStatus;
+import no.sikt.nva.nvi.common.model.PublicationDate;
 import no.sikt.nva.nvi.common.service.exception.CandidateNotFoundException;
 import no.sikt.nva.nvi.common.service.model.Candidate;
+import no.sikt.nva.nvi.test.SampleExpandedPublication;
 
 public class EvaluationSteps {
   private static final String OPEN_PERIOD = "OPEN";
@@ -36,36 +43,47 @@ public class EvaluationSteps {
     this.evaluationContext = new EvaluationContext(scenario);
   }
 
-  @Given("an unreported Candidate")
-  public void givenAnUnreportedCandidate() {
-    givenAnApplicablePublication();
-    givenTheReportingPeriodForThePublicationIs("OPEN");
-    whenThePublicationIsEvaluated();
-
-    assertPublicationIsUnreportedCandidate();
+  @Given("an applicable Publication published {string} year")
+  public void anApplicablePublicationPublishedYear(String relativeYear) {
+    var publicationDate = randomPublicationDateInYear(mapRelativeYearToValue(relativeYear));
+    setupApplicablePublication(publicationDate);
   }
 
-  @Given("an applicable Publication")
-  public void givenAnApplicablePublication() {
-    var publicationDate = randomPublicationDate();
-    var nviOrganization = publicationBuilder.setupTopLevelOrganization(COUNTRY_CODE_NORWAY, true);
-    publicationBuilder =
-        publicationBuilder
-            .withPublicationDate(publicationDate)
-            .withContributor(verifiedCreatorFrom(nviOrganization))
-            .withContributor(unverifiedCreatorFrom(nviOrganization));
+  @Given("an unreported Candidate for the Publication exists")
+  public void anUnreportedCandidateForThePublicationExists() {
+    setupCandidate(publicationBuilder.getExpandedPublication());
+
+    var candidate = assertPublicationIsCandidate();
+    assertThat(candidate.isReported()).isFalse();
   }
 
-  @Given("the reporting period for the Publication is {string}")
-  public void givenTheReportingPeriodForThePublicationIs(String periodState) {
-    var publicationDate = publicationBuilder.getExpandedPublication().publicationDate();
+  @Given("a reported Candidate for the Publication exists")
+  public void aReportedCandidateForThePublicationExists() {
+    setupCandidate(publicationBuilder.getExpandedPublication());
+    var candidate = assertPublicationIsCandidate();
+    setCandidateToReported(candidate);
+
+    var updatedCandidate = assertPublicationIsCandidate();
+    assertThat(updatedCandidate.isReported()).isTrue();
+  }
+
+  @Given("the reporting period for {string} year is {string}")
+  public void theReportingPeriodForYearIs(String relativeYear, String periodState) {
+    var publicationYear = mapRelativeYearToValue(relativeYear);
     switch (periodState) {
-      case CLOSED_PERIOD -> setupClosedPeriod(scenario, publicationDate.year());
-      case OPEN_PERIOD -> setupOpenPeriod(scenario, publicationDate.year());
-      case FUTURE_PERIOD -> setupFuturePeriod(scenario, publicationDate.year());
+      case CLOSED_PERIOD -> setupClosedPeriod(scenario, publicationYear);
+      case OPEN_PERIOD -> setupOpenPeriod(scenario, publicationYear);
+      case FUTURE_PERIOD -> setupFuturePeriod(scenario, publicationYear);
       case null, default ->
           throw new IllegalArgumentException("Invalid period state: " + periodState);
     }
+  }
+
+  @Given("the reporting period for {string} year is undefined")
+  public void theReportingPeriodForYearIsUndefined(String relativeYear) {
+    var publicationYear = String.valueOf(mapRelativeYearToValue(relativeYear));
+    var period = scenario.getPeriodService().findByPublishingYear(publicationYear);
+    assertThat(period).isEmpty();
   }
 
   @When("the Publication is evaluated")
@@ -74,8 +92,27 @@ public class EvaluationSteps {
         publicationBuilder.getExpandedPublication());
   }
 
+  @When("the Publication date is changed to {string} year")
+  public void thePublicationDateIsChangedToYear(String relativeYear) {
+    var publicationDate = randomPublicationDateInYear(mapRelativeYearToValue(relativeYear));
+    publicationBuilder = publicationBuilder.withPublicationDate(publicationDate);
+    evaluationContext.evaluatePublicationAndPersistResult(
+        publicationBuilder.getExpandedPublication());
+  }
+
+  @When("the Publication title is changed")
+  public void thePublicationTitleIsChanged() {
+    var updatedPublication =
+        publicationBuilder
+            .getExpandedPublicationBuilder()
+            .withTitle(randomString())
+            .withModifiedDate(Instant.now().toString())
+            .build();
+    evaluationContext.evaluatePublicationAndPersistResult(updatedPublication);
+  }
+
   @When("the Publication is updated to be non-applicable")
-  public void whenThePublicationTypeIsChangedSoThatThePublicationIsNoLongerApplicable2() {
+  public void whenThePublicationIsUpdatedToBeNonApplicable() {
     evaluationContext.evaluatePublicationAndPersistResult(
         publicationBuilder.withPublicationType("ComicBook").getExpandedPublication());
   }
@@ -99,16 +136,60 @@ public class EvaluationSteps {
 
   @Then("the Candidate is updated")
   public void thenTheCandidateIsUpdated() {
-    assertPublicationIsUnreportedCandidate();
     assertCandidateIsUpdated();
+    assertPublicationIsUnreportedCandidate();
+  }
+
+  @Then("the Candidate is not updated")
+  public void theCandidateIsNotUpdated() {
+    assertCandidateIsNotUpdated();
+  }
+
+  @Then("the Candidate is applicable")
+  public void theCandidateIsApplicable() {
+    var candidate = assertPublicationIsCandidate();
+    assertThat(candidate.isApplicable()).isTrue();
+  }
+
+  @Then("the reporting period for the Candidate is {string} year")
+  public void theReportingPeriodForTheCandidateIsYear(String relativeYear) {
+    var candidate = assertPublicationIsCandidate();
+    var year = mapRelativeYearToValue(relativeYear);
+    var candidatePeriod = candidate.getPeriod().orElseThrow();
+    assertThat(candidatePeriod.publishingYear()).isEqualTo(year);
+  }
+
+  private static int mapRelativeYearToValue(String yearName) {
+    return switch (yearName) {
+      case "previous", "last" -> CURRENT_YEAR - 1;
+      case "this" -> CURRENT_YEAR;
+      case "next" -> CURRENT_YEAR + 1;
+      default -> throw new IllegalStateException("Unexpected value: " + yearName);
+    };
+  }
+
+  private void setupApplicablePublication(PublicationDate publicationDate) {
+    var nviOrganization = publicationBuilder.setupTopLevelOrganization(COUNTRY_CODE_NORWAY, true);
+    publicationBuilder =
+        publicationBuilder
+            .withPublicationDate(publicationDate)
+            .withContributor(verifiedCreatorFrom(nviOrganization))
+            .withContributor(unverifiedCreatorFrom(nviOrganization));
+  }
+
+  private Candidate assertPublicationIsCandidate() {
+    var candidate = getCandidateByPublicationId();
+    assertThat(candidate)
+        .extracting(Candidate::getPublicationId, Candidate::isApplicable)
+        .containsExactly(publicationId, true);
+    assertThat(candidate.approvals()).isNotEmpty();
+    assertThat(candidate.getPeriod()).isNotEmpty();
+    return candidate;
   }
 
   private void assertPublicationIsUnreportedCandidate() {
-    var candidate = getCandidateByPublicationId();
-    assertThat(candidate)
-        .extracting(Candidate::getPublicationId, Candidate::isApplicable, Candidate::isReported)
-        .containsExactly(publicationId, true, false);
-    assertThat(candidate.approvals()).isNotEmpty();
+    var candidate = assertPublicationIsCandidate();
+    assertThat(candidate.isReported()).isFalse();
   }
 
   private void assertCandidateIsUpdated() {
@@ -117,6 +198,13 @@ public class EvaluationSteps {
 
     assertThat(candidate.createdDate()).isBefore(evaluationTimestamp);
     assertThat(candidate.modifiedDate()).isAfterOrEqualTo(evaluationTimestamp);
+  }
+
+  private void assertCandidateIsNotUpdated() {
+    var candidate = getCandidateByPublicationId();
+    var evaluationTimestamp = evaluationContext.getLastEvaluationTimestamp();
+
+    assertThat(candidate.modifiedDate()).isBefore(evaluationTimestamp);
   }
 
   private void assertPublicationIsNonCandidate() {
@@ -130,5 +218,32 @@ public class EvaluationSteps {
   private Candidate getCandidateByPublicationId() {
     var publication = publicationBuilder.getExpandedPublication();
     return scenario.getCandidateByPublicationId(publication.id());
+  }
+
+  private void setupCandidate(SampleExpandedPublication publication) {
+    var publicationYear = publication.publicationDate().year();
+    var period =
+        scenario
+            .getPeriodService()
+            .findByPublishingYear(publicationYear)
+            .orElse(setupOpenPeriod(scenario, publicationYear));
+
+    if (period.isOpen()) {
+      evaluationContext.evaluatePublicationAndPersistResult(publication);
+    } else {
+      setupOpenPeriod(scenario, publicationYear);
+      evaluationContext.evaluatePublicationAndPersistResult(publication);
+      scenario.getPeriodRepository().update(period.toDao());
+    }
+  }
+
+  private void setCandidateToReported(Candidate candidate) {
+    var candidateDao = candidate.toDao();
+    var dbCandidate = candidateDao.candidate().copy().reportStatus(ReportStatus.REPORTED).build();
+    var updatedCandidateDao = candidateDao.copy().candidate(dbCandidate).build();
+
+    scenario
+        .getCandidateRepository()
+        .updateCandidateAggregate(updatedCandidateDao, emptyList(), emptyList(), emptyList());
   }
 }
