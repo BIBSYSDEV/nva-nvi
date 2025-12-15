0e09e724f8b3e8be48da21e12efbc327474eff23
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-12-15 13:25:09 +0100

    feat: Add initial message models

---
+++ event-handlers/src/main/java/no/sikt/nva/nvi/events/batch/model/BatchJobMessage.java
@@ -0,0 +1,14 @@
+package no.sikt.nva.nvi.events.batch.model;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import no.unit.nva.commons.json.JsonSerializable;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+  @JsonSubTypes.Type(value = RefreshCandidateMessage.class, name = "REFRESH_CANDIDATE"),
+  @JsonSubTypes.Type(value = MigrateCandidateMessage.class, name = "MIGRATE_CANDIDATE"),
+  @JsonSubTypes.Type(value = RefreshPeriodMessage.class, name = "REFRESH_PERIOD")
+})
+public sealed interface BatchJobMessage extends JsonSerializable
+    permits RefreshCandidateMessage, MigrateCandidateMessage, RefreshPeriodMessage {}
---
+++ event-handlers/src/main/java/no/sikt/nva/nvi/events/batch/model/ReportingYearFilter.java
@@ -0,0 +1,17 @@
+package no.sikt.nva.nvi.events.batch.model;
+
+import static java.util.Collections.emptyList;
+import static java.util.Objects.requireNonNullElse;
+
+import java.util.List;
+
+public record ReportingYearFilter(List<String> reportingYears) {
+
+  public ReportingYearFilter {
+    reportingYears = requireNonNullElse(reportingYears, emptyList());
+  }
+
+  public boolean includesAllYears() {
+    return reportingYears.isEmpty();
+  }
+}
