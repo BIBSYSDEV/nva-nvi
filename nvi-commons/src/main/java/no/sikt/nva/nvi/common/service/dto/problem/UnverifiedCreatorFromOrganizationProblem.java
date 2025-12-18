a01ae1488ab1bcbcd4e58349cb8ae330328603b8
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-12-18 10:25:45 +0100

    build: Bump nva-commons to 2.4.6 (#594)
    Changes:
    
    - Bumps `nva-commons` version
    - Removes unused `jsonassert` dependency
    - Automatic reformatting changes (likely because of new Spotless
    version)

--- nvi-commons/src/main/java/no/sikt/nva/nvi/common/service/dto/problem/UnverifiedCreatorFromOrganizationProblem.java
+++ nvi-commons/src/main/java/no/sikt/nva/nvi/common/service/dto/problem/UnverifiedCreatorFromOrganizationProblem.java
@@ -9,7 +9,7 @@ public record UnverifiedCreatorFromOrganizationProblem(
     implements CandidateProblem {
   private static final String DEFAULT_TITLE = "Unverified contributor from this organization";
   private static final String DEFAULT_DESCRIPTION =
-      """
+"""
 At least one of the contributors from this user's organization is unverified. \
 Organizations affiliated with this contributor cannot approve or reject the publication as an NVI candidate, \
 or receive NVI points for it, until the contributor is verified or removed from the publication.\
