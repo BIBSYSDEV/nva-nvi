844b313efcb2192fe06a5dd9e5ccd1e16822adc0
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-01-13 13:40:16 +0100

    wip: Fix constructor

--- event-handlers/src/main/java/no/sikt/nva/nvi/events/batch/StartBatchJobHandler.java
+++ event-handlers/src/main/java/no/sikt/nva/nvi/events/batch/StartBatchJobHandler.java
@@ -1,13 +1,21 @@
 package no.sikt.nva.nvi.events.batch;
 
+import static no.sikt.nva.nvi.common.db.DynamoRepository.defaultDynamoClient;
+import static no.sikt.nva.nvi.common.service.NviPeriodService.defaultNviPeriodService;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import no.sikt.nva.nvi.common.db.CandidateRepository;
 import no.sikt.nva.nvi.common.queue.NviQueueClient;
 import no.sikt.nva.nvi.common.queue.QueueClient;
+import no.sikt.nva.nvi.common.service.NviPeriodService;
 import no.sikt.nva.nvi.events.batch.model.StartBatchJobRequest;
+import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
 
 // FIXME: Temporary suppression to split up PRs
 @SuppressWarnings("PMD.UnusedPrivateField")
@@ -59,5 +67,10 @@ public class StartBatchJobHandler implements RequestHandler<StartBatchJobRequest
   public Void handleRequest(StartBatchJobRequest input, Context context) {
     LOGGER.info("Processing batch job: {}", input);
     throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  @JacocoGenerated
+  private static EventBridgeClient defaultEventBridgeClient() {
+    return EventBridgeClient.builder().httpClientBuilder(UrlConnectionHttpClient.builder()).build();
   }
 }
