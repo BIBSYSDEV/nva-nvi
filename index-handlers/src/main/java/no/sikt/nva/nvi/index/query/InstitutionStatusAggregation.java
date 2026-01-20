2a79907724acc144246cd129bea33b4e0beb0668
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-01-20 16:05:13 +0100

    fix: Filter points by approval status (#608)
    Ticket: https://sikt.atlassian.net/browse/NP-50618
    
    This changes the aggregation query that calculates points by
    institution. Previously, we included points from candidates that were
    not rejected by anyone. Now we only include points from candidates that
    are approved by everyone.

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/query/InstitutionStatusAggregation.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/query/InstitutionStatusAggregation.java
@@ -2,14 +2,13 @@ package no.sikt.nva.nvi.index.query;
 
 import static no.sikt.nva.nvi.common.utils.JsonUtils.jsonPathOf;
 import static no.sikt.nva.nvi.common.utils.Validator.shouldNotBeBlank;
-import static no.sikt.nva.nvi.index.model.document.ApprovalStatus.REJECTED;
 import static no.sikt.nva.nvi.index.query.ApprovalQuery.approvalBelongsTo;
 import static no.sikt.nva.nvi.index.utils.AggregationFunctions.filterAggregation;
 import static no.sikt.nva.nvi.index.utils.AggregationFunctions.nestedAggregation;
 import static no.sikt.nva.nvi.index.utils.AggregationFunctions.sumAggregation;
 import static no.sikt.nva.nvi.index.utils.AggregationFunctions.termsAggregation;
+import static no.sikt.nva.nvi.index.utils.QueryFunctions.fieldValueQuery;
 import static no.sikt.nva.nvi.index.utils.QueryFunctions.mustMatch;
-import static no.sikt.nva.nvi.index.utils.QueryFunctions.mustNotMatch;
 import static no.sikt.nva.nvi.index.utils.SearchConstants.APPROVALS;
 import static no.sikt.nva.nvi.index.utils.SearchConstants.APPROVAL_STATUS;
 import static no.sikt.nva.nvi.index.utils.SearchConstants.GLOBAL_APPROVAL_STATUS;
@@ -19,6 +18,7 @@ import static no.sikt.nva.nvi.index.utils.SearchConstants.ORGANIZATION_SUMMARIES
 import static no.sikt.nva.nvi.index.utils.SearchConstants.POINTS;
 
 import java.util.Map;
+import no.sikt.nva.nvi.common.service.model.GlobalApprovalStatus;
 import org.opensearch.client.opensearch._types.aggregations.Aggregation;
 import org.opensearch.client.opensearch._types.aggregations.TermsAggregation;
 
@@ -63,7 +63,7 @@ public final class InstitutionStatusAggregation {
     var organizationAggregations =
         Map.of(
             POINTS,
-            aggregatePotentialAffiliationPoints(),
+            aggregateApprovedAffiliationPoints(),
             APPROVAL_STATUS,
             termsAggregation(SUMMARY_PATH, APPROVAL_STATUS),
             GLOBAL_APPROVAL_STATUS,
@@ -85,7 +85,7 @@ public final class InstitutionStatusAggregation {
             APPROVAL_STATUS,
             termsAggregation(APPROVALS, APPROVAL_STATUS),
             POINTS,
-            aggregatePotentialTopLevelPoints(),
+            aggregateApprovedTopLevelPoints(),
             GLOBAL_APPROVAL_STATUS,
             termsAggregation(APPROVALS, GLOBAL_APPROVAL_STATUS));
     return filterByTopLevelOrganization(topLevelOrganizationId, topLevelAggregations);
@@ -96,15 +96,21 @@ public final class InstitutionStatusAggregation {
     return filterAggregation(mustMatch(approvalBelongsTo(topLevelOrganizationId)), subAggregations);
   }
 
-  private static Aggregation aggregatePotentialTopLevelPoints() {
+  private static Aggregation aggregateApprovedTopLevelPoints() {
     return filterAggregation(
-        mustNotMatch(REJECTED.getValue(), jsonPathOf(APPROVALS, APPROVAL_STATUS)),
+        mustMatch(
+            fieldValueQuery(
+                jsonPathOf(APPROVALS, GLOBAL_APPROVAL_STATUS),
+                GlobalApprovalStatus.APPROVED.getValue())),
         Map.of(TOTAL, sumAggregation(APPROVALS, POINTS, INSTITUTION_POINTS)));
   }
 
-  private static Aggregation aggregatePotentialAffiliationPoints() {
+  private static Aggregation aggregateApprovedAffiliationPoints() {
     return filterAggregation(
-        mustNotMatch(REJECTED.getValue(), jsonPathOf(SUMMARY_PATH, APPROVAL_STATUS)),
+        mustMatch(
+            fieldValueQuery(
+                jsonPathOf(SUMMARY_PATH, GLOBAL_APPROVAL_STATUS),
+                GlobalApprovalStatus.APPROVED.getValue())),
         Map.of(TOTAL, sumAggregation(SUMMARY_PATH, POINTS)));
   }
 }
