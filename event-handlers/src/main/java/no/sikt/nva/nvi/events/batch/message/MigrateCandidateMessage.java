f7a7e5ff769776d7c016455dc88fda05726075fc
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-01-27 07:16:37 +0100

    refactor: Move job execution logic to work items

--- event-handlers/src/main/java/no/sikt/nva/nvi/events/batch/message/MigrateCandidateMessage.java
+++ event-handlers/src/main/java/no/sikt/nva/nvi/events/batch/message/MigrateCandidateMessage.java
@@ -1,5 +1,11 @@
 package no.sikt.nva.nvi.events.batch.message;
 
 import java.util.UUID;
+import no.sikt.nva.nvi.common.CandidateMigrationService;
 
-public record MigrateCandidateMessage(UUID candidateIdentifier) implements BatchJobMessage {}
+public record MigrateCandidateMessage(UUID candidateIdentifier) implements BatchJobMessage {
+
+  public void execute(CandidateMigrationService candidateMigrationService) {
+    candidateMigrationService.migrateCandidate(candidateIdentifier);
+  }
+}
