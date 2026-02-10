26745a78e3343f00024c1adde244a46fc7ca8064
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-02-10 14:13:57 +0100

    refactor: Simplify OpenAPI schema

---
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/model/report/UnitSummary.java
@@ -0,0 +1,9 @@
+package no.sikt.nva.nvi.index.model.report;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.net.URI;
+
+@JsonSerialize
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+public record UnitSummary(URI id, DirectAffiliationAggregation summary) {}
