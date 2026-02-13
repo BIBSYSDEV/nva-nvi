2c449da96681810d7cf4675582f65c2bc04c68ec
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-02-13 09:46:41 +0100

    refactor: Fix imports

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/utils/QueryFunctions.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/utils/QueryFunctions.java
@@ -18,7 +18,6 @@ import no.sikt.nva.nvi.index.model.document.ApprovalStatus;
 import org.opensearch.client.json.JsonData;
 import org.opensearch.client.opensearch._types.FieldValue;
 import org.opensearch.client.opensearch._types.query_dsl.BoolQuery;
-import org.opensearch.client.opensearch._types.query_dsl.BoolQuery.Builder;
 import org.opensearch.client.opensearch._types.query_dsl.MatchAllQuery;
 import org.opensearch.client.opensearch._types.query_dsl.NestedQuery;
 import org.opensearch.client.opensearch._types.query_dsl.Query;
@@ -81,7 +80,7 @@ public final class QueryFunctions {
 
   public static Query mustMatch(Query... queries) {
     return new Query.Builder()
-        .bool(new Builder().must(Arrays.stream(queries).toList()).build())
+        .bool(new BoolQuery.Builder().must(Arrays.stream(queries).toList()).build())
         .build();
   }
 
