82d11eb2c70d078da72f6a0a789b6b52994e798c
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-02-13 14:40:29 +0100

    fix: Add logging (#662)

--- nvi-commons/src/main/java/no/sikt/nva/nvi/common/validator/SectorMigrationService.java
+++ nvi-commons/src/main/java/no/sikt/nva/nvi/common/validator/SectorMigrationService.java
@@ -71,7 +71,8 @@ public class SectorMigrationService implements MigrationService {
     return attempt(identityServiceClient::getAllCustomers)
         .map(CustomerList::customers)
         .map(SectorMigrationService::collectToCustomerMap)
-        .orElseThrow(failure -> new RuntimeException("Could not fetch customers!"));
+        .orElseThrow(
+            failure -> new RuntimeException("Could not fetch customers!", failure.getException()));
   }
 
   private static Map<URI, CustomerDto> collectToCustomerMap(List<CustomerDto> list) {
