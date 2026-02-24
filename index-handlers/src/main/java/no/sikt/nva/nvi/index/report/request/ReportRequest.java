1fa18e25df8b397bc205d8408c11b2a2e6f7bb6b
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-02-24 12:48:46 +0100

    refactor: Add queryId to interface

--- index-handlers/src/main/java/no/sikt/nva/nvi/index/report/request/ReportRequest.java
+++ index-handlers/src/main/java/no/sikt/nva/nvi/index/report/request/ReportRequest.java
@@ -1,7 +1,12 @@
 package no.sikt.nva.nvi.index.report.request;
 
+import java.net.URI;
+
 public sealed interface ReportRequest
     permits AllPeriodsReportRequest,
         PeriodReportRequest,
         AllInstitutionsReportRequest,
-        InstitutionReportRequest {}
+        InstitutionReportRequest {
+
+  URI queryId();
+}
