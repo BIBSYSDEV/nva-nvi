d53fcb9e3d6d33b7a28f8104b8427292ba7259db
Kir Truhacev <kir.truhacev@sikt.no>
2026-09-15 10:55:28 +0200

    migrating channel by api call to channel-registry

--- libs/migration-service/src/main/java/no/sikt/nva/nvi/migration/PublicationChannelMigrationService.java
+++ libs/migration-service/src/main/java/no/sikt/nva/nvi/migration/PublicationChannelMigrationService.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import java.util.UUID;
 import no.sikt.nva.nvi.common.S3StorageReader;
 import no.sikt.nva.nvi.common.StorageReader;
+import no.sikt.nva.nvi.common.client.PublicationChannelRetriever;
 import no.sikt.nva.nvi.common.dto.PublicationChannelDto;
 import no.sikt.nva.nvi.common.dto.PublicationDto;
 import no.sikt.nva.nvi.common.model.PointCalculation;
@@ -17,6 +18,7 @@ import no.sikt.nva.nvi.common.model.PublicationChannel;
 import no.sikt.nva.nvi.common.service.CandidateService;
 import no.sikt.nva.nvi.common.service.model.Candidate;
 import no.sikt.nva.nvi.publication.PublicationLoaderService;
+import no.unit.nva.auth.uriretriever.UriRetriever;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 import org.slf4j.Logger;
@@ -28,35 +30,49 @@ public final class PublicationChannelMigrationService implements MigrationServic
       LoggerFactory.getLogger(PublicationChannelMigrationService.class);
 
   private static final String EXPANDED_RESOURCES_BUCKET = "EXPANDED_RESOURCES_BUCKET";
+  private static final String MESSAGE_MIGRATION_NOT_NEEDED =
+      "Candidate {} does not require migration";
+  private static final String MESSAGE_MIGRATING_CANDIDATE =
+      "Migrating candidate with identifier {}";
+  private static final String MESSAGE_CHANNEL_NOT_IN_PUBLICATION =
+      "Channel {} of candidate {} is not in the current publication, fetching channel metadata";
+  private static final String MESSAGE_NO_CHANNEL_METADATA_FOUND =
+      "Found no metadata for channel {} of candidate {}";
   private final CandidateService candidateService;
   private final PublicationLoaderService publicationLoader;
+  private final PublicationChannelRetriever channelRetriever;
 
   public PublicationChannelMigrationService(
-      CandidateService candidateService, StorageReader<URI> storageReader) {
+      CandidateService candidateService,
+      StorageReader<URI> storageReader,
+      PublicationChannelRetriever channelRetriever) {
     this.candidateService = candidateService;
     this.publicationLoader = new PublicationLoaderService(storageReader);
+    this.channelRetriever = channelRetriever;
   }
 
   @JacocoGenerated
   public static PublicationChannelMigrationService defaultService() {
     return new PublicationChannelMigrationService(
         defaultCandidateService(),
-        new S3StorageReader(new Environment().readEnv(EXPANDED_RESOURCES_BUCKET)));
+        new S3StorageReader(new Environment().readEnv(EXPANDED_RESOURCES_BUCKET)),
+        new PublicationChannelRetriever(new UriRetriever()));
   }
 
   @Override
   public void migrateCandidate(UUID identifier) {
     var candidate = candidateService.getCandidateByIdentifier(identifier);
-    if (!hasMissingChannelMetadata(candidate.getPublicationChannel())) {
-      LOGGER.info("Candidate {} does not require migration", identifier);
+    var currentChannel = candidate.getPublicationChannel();
+    if (!hasMissingChannelMetadata(currentChannel)) {
+      LOGGER.info(MESSAGE_MIGRATION_NOT_NEEDED, identifier);
       return;
     }
 
-    LOGGER.info("Migrating candidate with identifier {}", identifier);
+    LOGGER.info(MESSAGE_MIGRATING_CANDIDATE, identifier);
     var publicationBucketUri = candidate.publicationDetails().publicationBucketUri();
     var publication = publicationLoader.extractAndTransform(publicationBucketUri);
-    var updatedChannel = addMissingChannelMetadata(candidate.getPublicationChannel(), publication);
-    if (!updatedChannel.equals(candidate.getPublicationChannel())) {
+    var updatedChannel = addMissingChannelMetadata(currentChannel, publication, identifier);
+    if (!updatedChannel.equals(currentChannel)) {
       candidateService.updateCandidate(withChannel(candidate, updatedChannel));
     }
   }
@@ -85,28 +101,46 @@ public final class PublicationChannelMigrationService implements MigrationServic
         pointCalculation.totalPoints());
   }
 
-  private static PublicationChannel addMissingChannelMetadata(
-      PublicationChannel channel, PublicationDto publication) {
-    return findChannelById(publication, channel.id())
+  private PublicationChannel addMissingChannelMetadata(
+      PublicationChannel channel, PublicationDto publication, UUID identifier) {
+    return findChannelInPublication(publication, channel.id())
+        .map(current -> withMetadata(channel, current.name(), current.printIssn()))
+        .or(() -> fetchChannelMetadata(channel, identifier))
+        .orElseGet(() -> logMissingMetadata(channel, identifier));
+  }
+
+  private Optional<PublicationChannel> fetchChannelMetadata(
+      PublicationChannel channel, UUID identifier) {
+    LOGGER.info(MESSAGE_CHANNEL_NOT_IN_PUBLICATION, channel.id(), identifier);
+    return channelRetriever
+        .fetchChannel(channel.id())
+        .filter(fetchedChannel -> !isBlank(fetchedChannel.name()))
         .map(
-            current ->
-                new PublicationChannel(
-                    channel.id(),
-                    channel.channelType(),
-                    channel.scientificValue(),
-                    current.name(),
-                    current.printIssn()))
-        .orElse(channel);
+            fetchedChannel ->
+                withMetadata(channel, fetchedChannel.name(), fetchedChannel.printIssn()));
+  }
+
+  private static PublicationChannel logMissingMetadata(
+      PublicationChannel channel, UUID identifier) {
+    LOGGER.info(MESSAGE_NO_CHANNEL_METADATA_FOUND, channel.id(), identifier);
+    return channel;
+  }
+
+  private static PublicationChannel withMetadata(
+      PublicationChannel channel, String name, String printIssn) {
+    return new PublicationChannel(
+        channel.id(), channel.channelType(), channel.scientificValue(), name, printIssn);
   }
 
   private static boolean hasMissingChannelMetadata(PublicationChannel channel) {
     return nonNull(channel) && nonNull(channel.id()) && isBlank(channel.name());
   }
 
-  private static Optional<PublicationChannelDto> findChannelById(
+  private static Optional<PublicationChannelDto> findChannelInPublication(
       PublicationDto publication, URI channelId) {
     return publication.publicationChannels().stream()
         .filter(channel -> channelId.equals(channel.id()))
+        .filter(channel -> !isBlank(channel.name()))
         .findAny();
   }
 }
--- libs/migration-service/src/test/java/no/sikt/nva/nvi/migration/PublicationChannelMigrationServiceTest.java
+++ libs/migration-service/src/test/java/no/sikt/nva/nvi/migration/PublicationChannelMigrationServiceTest.java
@@ -1,5 +1,7 @@
 package no.sikt.nva.nvi.migration;
 
+import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
+import static java.net.HttpURLConnection.HTTP_OK;
 import static no.sikt.nva.nvi.common.db.CandidateDaoFixtures.createCandidateInRepository;
 import static no.sikt.nva.nvi.common.db.DbCandidateFixtures.randomCandidateBuilder;
 import static no.sikt.nva.nvi.common.db.DbPointCalculationFixtures.randomPointCalculationBuilder;
@@ -17,19 +19,41 @@ import java.time.Instant;
 import java.util.UUID;
 import no.sikt.nva.nvi.common.SampleExpandedPublicationFactory;
 import no.sikt.nva.nvi.common.TestScenario;
+import no.sikt.nva.nvi.common.client.PublicationChannelRetriever;
 import no.sikt.nva.nvi.common.db.CandidateRepository;
 import no.sikt.nva.nvi.common.db.ReportStatus;
 import no.sikt.nva.nvi.common.db.model.DbPublicationChannel;
 import no.sikt.nva.nvi.common.service.CandidateService;
 import no.sikt.nva.nvi.test.SampleExpandedPublication;
+import no.sikt.nva.nvi.test.uriretriever.FakeUriRetriever;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PublicationChannelMigrationServiceTest {
 
+  private static final String APPLICATION_JSON = "application/json";
+  private static final String CHANNEL_NAME_IN_REGISTRY = "Abasyn Journal of Life Sciences (AJLS)";
+  private static final String CHANNEL_PRINT_ISSN_IN_REGISTRY = "2616-9754";
+  private static final String CHANNEL_RESPONSE_TEMPLATE =
+      """
+      {
+        "id" : "%s",
+        "identifier" : "81543BCB-50FD-4277-8457-28E5674BA406",
+        "name" : "%s",
+        "onlineIssn" : "2663-1040",
+        "printIssn" : "%s",
+        "scientificValue" : "LevelOne",
+        "sameAs" : "https://kanalregister.hkdir.no/publiseringskanaler/info/tidsskrift?pid=81543BCB-50FD-4277-8457-28E5674BA406",
+        "type" : "Journal",
+        "year" : "2024",
+        "@context" : "https://bibsysdev.github.io/src/publication-channel/channel-context.json"
+      }
+      """;
+
   private TestScenario scenario;
   private CandidateService candidateService;
   private CandidateRepository candidateRepository;
+  private FakeUriRetriever uriRetriever;
   private PublicationChannelMigrationService migrationService;
   private SampleExpandedPublicationFactory publicationFactory;
 
@@ -39,7 +63,10 @@ class PublicationChannelMigrationServiceTest {
     candidateService = scenario.getCandidateService();
     candidateRepository = scenario.getCandidateRepository();
     var storageReader = scenario.getS3StorageReaderForExpandedResourcesBucket();
-    migrationService = new PublicationChannelMigrationService(candidateService, storageReader);
+    uriRetriever = FakeUriRetriever.newInstance();
+    migrationService =
+        new PublicationChannelMigrationService(
+            candidateService, storageReader, new PublicationChannelRetriever(uriRetriever));
     setupOpenPeriod(scenario, CURRENT_YEAR);
     publicationFactory = new SampleExpandedPublicationFactory();
   }
@@ -81,9 +108,42 @@ class PublicationChannelMigrationServiceTest {
   }
 
   @Test
-  void shouldKeepPersistedCandidateChannelWhenPublicationHasAnotherChannel() {
+  void shouldFetchChannelMetadataWhenPublicationHasAnotherChannel() {
+    var publication = publicationFactory.getExpandedPublication();
+    var reportedChannel = channelWithoutMetadata(randomUri());
+    registerChannelResponse(reportedChannel.id());
+    var candidateId = createLegacyCandidateWithChannel(publication, reportedChannel);
+
+    migrationService.migrateCandidate(candidateId);
+
+    var migratedChannel =
+        candidateService.getCandidateByIdentifier(candidateId).getPublicationChannel();
+    assertThat(migratedChannel.id()).isEqualTo(reportedChannel.id());
+    assertThat(migratedChannel.name()).isEqualTo(CHANNEL_NAME_IN_REGISTRY);
+    assertThat(migratedChannel.printIssn()).isEqualTo(CHANNEL_PRINT_ISSN_IN_REGISTRY);
+  }
+
+  @Test
+  void shouldKeepScientificValueAndTypeWhenFetchingChannelMetadata() {
+    var publication = publicationFactory.getExpandedPublication();
+    var reportedChannel = channelWithoutMetadata(randomUri());
+    registerChannelResponse(reportedChannel.id());
+    var candidateId = createLegacyCandidateWithChannel(publication, reportedChannel);
+
+    migrationService.migrateCandidate(candidateId);
+
+    var migratedChannel =
+        candidateService.getCandidateByIdentifier(candidateId).getPublicationChannel();
+    assertThat(migratedChannel.scientificValue().getValue())
+        .isEqualTo(reportedChannel.scientificValue());
+    assertThat(migratedChannel.channelType().getValue()).isEqualTo(reportedChannel.channelType());
+  }
+
+  @Test
+  void shouldKeepPersistedChannelWhenChannelCannotBeFetched() {
     var publication = publicationFactory.getExpandedPublication();
     var replacedChannelId = randomUri();
+    uriRetriever.registerResponse(replacedChannelId, HTTP_NOT_FOUND, APPLICATION_JSON, "");
     var candidateId =
         createLegacyCandidateWithChannel(publication, channelWithoutMetadata(replacedChannelId));
 
@@ -94,6 +154,13 @@ class PublicationChannelMigrationServiceTest {
     assertThat(migratedChannel.id()).isEqualTo(replacedChannelId);
     assertThat(migratedChannel.name()).isNull();
     assertThat(migratedChannel.printIssn()).isNull();
+  }
+
+  private void registerChannelResponse(URI channelId) {
+    var body =
+        CHANNEL_RESPONSE_TEMPLATE.formatted(
+            channelId, CHANNEL_NAME_IN_REGISTRY, CHANNEL_PRINT_ISSN_IN_REGISTRY);
+    uriRetriever.registerResponse(channelId, HTTP_OK, APPLICATION_JSON, body);
   }
 
   private static DbPublicationChannel channelWithoutMetadata(URI channelId) {
